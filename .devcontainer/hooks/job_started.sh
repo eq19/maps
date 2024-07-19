8bb1d195ad6dcd6a3181e6ae205acb44424a8787
eQuantum <8466209+eq19@users.noreply.github.com>
2024-07-19 19:44:30 +0700

    Update job_started.sh

--- .devcontainer/hooks/job_started.sh
+++ .devcontainer/hooks/job_started.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 # Structure: Cell Types – Modulo 6
 
-#export hr='%.s─' $(seq 1 $(tput cols))
+export RED=$(tput setaf 1 :-"" 2>/dev/null)
+export GREEN=$(tput setaf 2 :-"" 2>/dev/null)
+export YELLOW=$(tput setaf 3 :-"" 2>/dev/null)
+export BLUE=$(tput setaf 4 :-"" 2>/dev/null)
+export RESET=$(tput sgr0 :-"" 2>/dev/null)
+
+echo $GREEN some text $RESET
+echo $RED; printf -- "-%.0s" $(seq $(tput cols)); echo $RESET
+echo $YELLOW more text $RESET
 
 echo -e "\n$hr\nIdentity\n$hr"
 whoami
 pwd
 id
 
-echo -e "\n$hr\nSupervisor\n$hr"
-apt-cache show supervisor
-
 echo -e "\n$hr\nService Status\n$hr"
 service --status-all
+
+echo -e "\n$hr\nSupervisor\n$hr"
+apt-cache show supervisor
 
 echo -e "\n$hr\nLocate Initdb\n$hr"
 locate initdb
