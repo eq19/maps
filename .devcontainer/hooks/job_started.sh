c1c9cb8709ecd3219da2c41eca3b57af97a34e9b
eQuantum <8466209+eq19@users.noreply.github.com>
2024-07-19 13:25:20 +0700

    Update job_started.sh

--- .devcontainer/hooks/job_started.sh
+++ .devcontainer/hooks/job_started.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 # Structure: Cell Types – Modulo 6
 
+hr=--------------------------------------------------------------------------------
+echo -e "\n$hr\nIdentity\n$hr"
+whoami
+pwd
+id
+
+echo -e "\n$hr\nSupervisor\n$hr"
 apt-cache show supervisor
+
+echo -e "\n$hr\nService Status\n$hr"
 service --status-all
+
+echo -e "\n$hr\nLocate Initdb\n$hr"
 locate initdb
