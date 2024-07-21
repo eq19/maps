70cdc17c433142b50e68809cd34e2eb40f444852
eQuantum <8466209+eq19@users.noreply.github.com>
2024-07-21 12:36:28 +0700

    Update job_started.sh

--- .devcontainer/hooks/job_started.sh
+++ .devcontainer/hooks/job_started.sh
@@ -20,6 +20,7 @@ echo -e "\n$hr\nService Status\n$hr"
 service --status-all
 
 echo -e "\n$hr\nOperation System\n$hr"
+cat /etc/os-release
 
 echo -e "\n$hr\nSupervisor\n$hr"
 apt-cache show supervisor
@@ -34,10 +35,10 @@ echo -e "\n$hr\nDB Cluster\n$hr"
 /etc/init.d/postgresql restart
 
 echo -e "\n$hr\nDisk Structure\n$hr"
-
+df -h
 
 echo -e "\n$hr\nExecutables\n$hr"
-
+find ${PATH//:/ } -maxdepth 1 -executable | sort
 
 echo -e "\n$hr\nDockerfile\n$hr"
 
