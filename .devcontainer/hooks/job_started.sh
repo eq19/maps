e76a2b97adeaae4d5b5f1426aab7fcfa791b6822
eQuantum <8466209+eq19@users.noreply.github.com>
2024-07-31 03:40:08 +0700

    Update job_started.sh

--- .devcontainer/hooks/job_started.sh
+++ .devcontainer/hooks/job_started.sh
@@ -30,9 +30,6 @@ apt-cache show supervisor
 echo -e "\n$hr\nEnvironment\n$hr"
 printenv | sort
 
-echo -e "\n$hr\nDockerfile\n$hr"
-find / -type f -name "Dockerfile" | sort
-
 echo -e "\n$hr\nExecutables\n$hr"
 find ${PATH//:/ } -maxdepth 1 -executable | sort
 
@@ -41,24 +38,27 @@ if [ -d /mnt/disks/Linux/usr/local/sbin ]; then
   echo -e "\n$hr\n"
   find /mnt/disks/Linux -maxdepth 3 -executable | sort 
   
-  echo -e "\n$hr\nPackage List\n$hr"
-  /mnt/disks/Linux/usr/bin/dpkg -l | sort
+  #echo -e "\n$hr\nPackage List\n$hr"
+  #/mnt/disks/Linux/usr/bin/dpkg -l | sort
 
-  echo -e "\n$hr\nPython Modules\n$hr"
-  /mnt/disks/Linux/usr/bin/python3 -c 'help("modules")'
+  #echo -e "\n$hr\nPython Modules\n$hr"
+  #/mnt/disks/Linux/usr/bin/python3 -c 'help("modules")'
 
-  echo -e "\n$hr\nLocate Python\n$hr" 
-  find /mnt/disks/Linux -type d -name '*python*' | sort
+  #echo -e "\n$hr\nLocate Python\n$hr" 
+  #find /mnt/disks/Linux -type d -name '*python*' | sort
 
-  echo -e "\n$hr\nTensorflow\n$hr"
-  find /mnt/disks/Linux -type d -name "tensorflow*" | sort
+  #echo -e "\n$hr\nTensorflow\n$hr"
+  #find /mnt/disks/Linux -type d -name "tensorflow*" | sort
 
-  echo -e "\n$hr\nLocate Requirements\n$hr" 
-  locate requirements.txt
-  echo -e "\n$hr\n"
-  find /mnt/disks/Linux -type f -name "requirements*.txt" | sort
+  #echo -e "\n$hr\nLocate Requirements\n$hr" 
+  #locate requirements.txt
+  #echo -e "\n$hr\n"
+  #find /mnt/disks/Linux -type f -name "requirements*.txt" | sort
 
-  echo -e "\n$hr\nLocate SQL\n$hr" 
-  find /mnt/disks/Linux -type f -name "*.sql" | sort
+  #echo -e "\n$hr\nDockerfile\n$hr"
+  #find / -type f -name "Dockerfile" | sort
+
+  #echo -e "\n$hr\nLocate SQL\n$hr" 
+  #find /mnt/disks/Linux -type f -name "*.sql" | sort
 
 fi
