e4393657b1a5430d776319d05871f39e8c6a2fdd
eQuantum <8466209+eq19@users.noreply.github.com>
2024-08-05 01:49:22 +0700

    Update job_completed.sh

--- .devcontainer/hooks/job_completed.sh
+++ .devcontainer/hooks/job_completed.sh
@@ -14,3 +14,8 @@ if [ -d /mnt/disks/Linux/usr/bin ]; then
 fi
 
 echo -e "\njob completed"
+HEADER="Accept: application/vnd.github+json"
+echo ${{ secrets.ACCESS_TOKEN }} | gh auth login --with-token
+TOTAL_COUNT=$(gh api -H "${HEADER}" /repos/${REPO}/actions/runners --jq '.total_count')
+RUNNER_ID=$(gh api -H "${HEADER}" /repos/${REPO}/actions/runners --jq '.runners.[].id')
+if (( $TOTAL_COUNT != 0 )); then gh api --method DELETE -H "${HEADER}" /repos/${REPO}/actions/runners/${RUNNER_ID}; fi 
