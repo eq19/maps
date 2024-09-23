e371573d409379d68796fa0b57728a750888f6e0
eq19 <eq19@users.noreply.github.com>
2024-09-23 13:21:45 +0000

    Assign eQuantum

--- .github/entrypoint/artifact.sh
+++ .github/entrypoint/artifact.sh
@@ -118,9 +118,6 @@ jekyll_build() {
   cp -R ${RUNNER_TEMP}/gistdir/* . && mkdir ${RUNNER_TEMP}/workdir/_data
   mv -f ${RUNNER_TEMP}/orgs.json ${RUNNER_TEMP}/workdir/_data/orgs.json
            
-  git clone --single-branch -b gh-pages https://github.com/${OWNER}/$1 ${RUNNER_TEMP}/$1 &>/dev/null
-  [[ -f ${RUNNER_TEMP}/$1/.jekyll-metadata ]] && mv -f ${RUNNER_TEMP}/$1/.jekyll-metadata .
-  [[ -d ${RUNNER_TEMP}/$1/.sass-cache ]] && mv -f ${RUNNER_TEMP}/$1/.sass-cache .
 }
 
 # Get structure on gist files
