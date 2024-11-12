a09b58531b3a89a1c4adae003710a40f6d2fa63f
eq19 <eq19@users.noreply.github.com>
2024-11-12 15:10:54 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -19,7 +19,7 @@ REMOTE_REPO="https://${GITHUB_ACTOR}:${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}
 
 API_URL="https://api.github.com/users/eq19/events/public"
 LATEST_COMMIT=$(curl -s $API_URL | jq ".[0].payload.commits[0].message")
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
   if [[ -z "$LATEST_COMMIT" ]]; then
     echo 'LATEST_COMMIT="update by workspace"' >> ${GITHUB_ENV}
   elif [[ "$LATEST_COMMIT" == null ]]; then
@@ -31,10 +31,11 @@ else
   echo 'LATEST_COMMIT="update by workspace"' >> ${GITHUB_ENV}
 fi
 
-if [[ "$RUNNER_OS" == "Linux" ]]; then
+if [[ "${JOB_ID}" == "1" ]]; then
    
   cd "${GITHUB_WORKSPACE}" && rm -rf .github
-  cp -r /home/runner/work/_actions/eq19/eq19/v1/.github . && chown -R "$(whoami)" .github
+  cp -r /home/runner/work/_actions/eq19/eq19/v1/.github .
+  chown -R "$(whoami)" .github
 
   git remote set-url origin ${REMOTE_REPO}        
   git add . && git commit -m "update workflows" && git push
