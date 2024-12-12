54bf9eb12de5c262c02fc2fa61c461a6548a8b96
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-12 10:13:43 +0700

    Update utilities.py

--- numberGenerator/python/utilities.py
+++ numberGenerator/python/utilities.py
@@ -60,7 +60,7 @@ def get_spin_nums(num, pfile):
     c_nums = [Decimal(i) for i in c_line]
     n_nums = [Decimal(i) for i in n_line]
 
-    print (pfile.name)
+    #print (pfile.name)
 
     if(c_nums[0] > num):
         return (1, 1)
