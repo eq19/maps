7d435b1451c4de9be4b65c5772c2d04105fd2571
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-15 08:40:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ if __name__ == '__main__':
         print(f"ERROR: no path to Main.class was provided")
         exit(1)
     file_path, *args = args
-    clazz = parse_class_file(file_path)
-    [main] = find_methods_by_name(clazz, b'main')
-    [code] = find_attributes_by_name(clazz, main['attributes'], b'Code')
-    code_attrib = parse_code_info(code['info'])
-    execute_code(clazz, code_attrib['code'])
+    clazz = jello.parse_class_file(file_path)
+    [main] = jello.find_methods_by_name(clazz, b'main')
+    [code] = jello.find_attributes_by_name(clazz, main['attributes'], b'Code')
+    code_attrib = jello.parse_code_info(code['info'])
+    jello.execute_code(clazz, code_attrib['code'])
   
