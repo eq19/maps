e28d4faee5e55708c5bdd600ad84d375b7ec29ba
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-15 08:56:07 +0700

    Update Main.java

--- javaCode/Main.java
+++ javaCode/Main.java
@@ -3,7 +3,11 @@ public abstract class Main {
     public static void foo(float x) {  }
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("************************************");
+        System.out.println("  Prime Hexagon Near Pi");
+        System.out.println("  by eQuantum Project");
+        System.out.println("************************************");
+        System.out.println(""); 
         System.out.println(34 + 35);
     }
 }
