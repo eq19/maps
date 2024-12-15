c306f67c1951fa1c767ddaf8990c38c2c59b7155
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-15 11:28:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -78,14 +78,16 @@ for num in finished:
 results.close()
 
 # Print how long it took
-#print(datetime.now() - start_time)if __name__ == '__main__':
-    program, *args = sys.argv
-    if len(args) == 0:
-        print(f"Usage: {program} <path/to/Main.class>")
-        print(f"ERROR: no path to Main.class was provided")
-        exit(1)
-    file_path, *args = args
-    clazz = jello.parse_class_file(file_path)
+#print(datetime.now() - start_time)
+
+if __name__ == '__main__':
+    #program, *args = sys.argv
+    #if len(args) == 0:
+        #print(f"Usage: {program} <path/to/Main.class>")
+        #print(f"ERROR: no path to Main.class was provided")
+        #exit(1)
+    #file_path, *args = args
+    clazz = jello.parse_class_file('javaCode/Main.class')
     [main] = jello.find_methods_by_name(clazz, b'main')
     [code] = jello.find_attributes_by_name(clazz, main['attributes'], b'Code')
     code_attrib = jello.parse_code_info(code['info'])
