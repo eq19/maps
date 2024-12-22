261f49628818c81ba538e601b555ecfbfdb1bd8f
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-22 21:34:54 +0700

    Update SampleStrategy.py

--- dataFile/user_data/strategies/SampleStrategy.py
+++ dataFile/user_data/strategies/SampleStrategy.py
@@ -1,140 +1,30 @@
-
-# --- Do not remove these libs ---
-from freqtrade.strategy import IStrategy
-from freqtrade.strategy import CategoricalParameter, IntParameter
-from functools import reduce
+from freqtrade.strategy.interface import IStrategy
 from pandas import DataFrame
-# --------------------------------
-
-import talib.abstract as ta
-import freqtrade.vendor.qtpylib.indicators as qtpylib
-import numpy # noqa
-
 
 class SampleStrategy(IStrategy):
-    """
-    Strategy 005
-    author@: Gerald Lonlas
-    github@: https://github.com/freqtrade/freqtrade-strategies
-
-    How to use it?
-    > python3 ./freqtrade/main.py -s Strategy005
-    """
-    INTERFACE_VERSION = 3
-
-    # Minimal ROI designed for the strategy.
-    # This attribute will be overridden if the config file contains "minimal_roi"
+    # Define strategy parameters
     minimal_roi = {
-        "1440": 0.01,
-        "80": 0.02,
-        "40": 0.03,
-        "20": 0.04,
-        "0":  0.05
+        "0": 0.1  # 10% profit
     }
-
-    # Optimal stoploss designed for the strategy
-    # This attribute will be overridden if the config file contains "stoploss"
-    stoploss = -0.10
-
-    # Optimal timeframe for the strategy
-    timeframe = '15m'
-
-    # trailing stoploss
-    trailing_stop = False
-    trailing_stop_positive = 0.01
-    trailing_stop_positive_offset = 0.02
-
-    # run "populate_indicators" only for new candle
-    process_only_new_candles = True
-
-    # Experimental settings (configuration will overide these if set)
-    use_exit_signal = True
-    exit_profit_only = True
-    ignore_roi_if_entry_signal = False
-
-    # Optional order type mapping
-    order_types = {
-        'entry': 'limit',
-        'exit': 'limit',
-        'stoploss': 'market',
-        'stoploss_on_exchange': False
-    }
-
-    buy_volumeAVG = IntParameter(low=50, high=300, default=70, space='buy', optimize=True)
-    buy_rsi = IntParameter(low=1, high=100, default=30, space='buy', optimize=True)
-    buy_fastd = IntParameter(low=1, high=100, default=30, space='buy', optimize=True)
-    buy_fishRsiNorma = IntParameter(low=1, high=100, default=30, space='buy', optimize=True)
-
-    sell_rsi = IntParameter(low=1, high=100, default=70, space='sell', optimize=True)
-    sell_minusDI = IntParameter(low=1, high=100, default=50, space='sell', optimize=True)
-    sell_fishRsiNorma = IntParameter(low=1, high=100, default=50, space='sell', optimize=True)
-    sell_trigger = CategoricalParameter(["rsi-macd-minusdi", "sar-fisherRsi"],
-                                        default=30, space='sell', optimize=True)
-
-    # Buy hyperspace params:
-    buy_params = {
-        "buy_fastd": 1,
-        "buy_fishRsiNorma": 5,
-        "buy_rsi": 26,
-        "buy_volumeAVG": 150,
-    }
-
-    # Sell hyperspace params:
-    sell_params = {
-        "sell_fishRsiNorma": 30,
-        "sell_minusDI": 4,
-        "sell_rsi": 74,
-        "sell_trigger": "rsi-macd-minusdi",
-    }
-
-    def informative_pairs(self):
-        """
-        Define additional, informative pair/interval combinations to be cached from the exchange.
-        These pair/interval combinations are non-tradeable, unless they are part
-        of the whitelist as well.
-        For more information, please consult the documentation
-        :return: List of tuples in the format (pair, interval)
-            Sample: return [("ETH/USDT", "5m"),
-                            ("BTC/USDT", "15m"),
-                            ]
-        """
-        return []
-
-    def informative_pairs(self):
-        """
-        Define additional, informative pair/interval combinations to be cached from the exchange.
-        These pair/interval combinations are non-tradeable, unless they are part
-        of the whitelist as well.
-        For more information, please consult the documentation
-        :return: List of tuples in the format (pair, interval)
-            Sample: return [("ETH/USDT", "5m"),
-                            ("BTC/USDT", "15m"),
-                            ]
-        """
-        return []
+    stoploss = -0.2  # 20% stop loss
+    timeframe = '1h'
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        """
-        Adds several different TA indicators to the given DataFrame
-
-        Performance Note: For the best performance be frugal on the number of indicators
-        you are using. Let uncomment only the indicator you are using in your strategies
-        or your hyperopt configuration, otherwise you will waste your memory and CPU usage.
-        """
+        # Add SMA indicators
+        dataframe['sma_short'] = dataframe['close'].rolling(window=10).mean()
+        dataframe['sma_long'] = dataframe['close'].rolling(window=50).mean()
         return dataframe
 
-    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        """
-        Based on TA indicators, populates the buy signal for the given dataframe
-        :param dataframe: DataFrame
-        :return: DataFrame with buy column
-        """
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        # Buy when short SMA crosses above long SMA
+        dataframe.loc[
+            (dataframe['sma_short'] > dataframe['sma_long']),
+            'buy'] = 1
         return dataframe
 
-    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        """
-        Based on TA indicators, populates the sell signal for the given dataframe
-        :param dataframe: DataFrame
-        :return: DataFrame with buy column
-        """
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        # Sell when short SMA crosses below long SMA
+        dataframe.loc[
+            (dataframe['sma_short'] < dataframe['sma_long']),
+            'sell'] = 1
         return dataframe
