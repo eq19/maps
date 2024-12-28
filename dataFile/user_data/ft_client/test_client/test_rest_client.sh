41f2de8d35727da695b03e12b6a593f297bc24da
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-28 21:24:49 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -1,1 +1,5 @@
-
+cd ${{ github.workspace }}
+python user_data/ft_client/test_client/test_client.py
+        
+#Ref: https://medium.com/@shanejones/how-i-set-up-freqtrade-a287db8966f
+freqtrade trade --config user_data/config_examples/config_indodax.example.json
