1eccbbe76ce52d4c95576b134e88690a337c60ae
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-29 14:40:03 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Structure: Cell Types – Modulo 6
 
+hr='------------------------------------------------------------------------------------'
+
 python user_data/ft_client/test_client/test_client.py
         
 #Ref: https://medium.com/@shanejones/how-i-set-up-freqtrade-a287db8966f
