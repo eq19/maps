d26c679d4056d2dd18c3b4823f20c5b2c313943b
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-29 14:44:33 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # Structure: Cell Types – Modulo 6
+# Ref: https://github.com/ccxt/ccxt/blob/4.4.40/python/ccxt/async_support/indodax.py#L195
 
 hr='------------------------------------------------------------------------------------'
 
+#cd ${{ github.workspace }}/user_data/build_helpers && ./install_ta-lib.sh > /dev/null 2>&1
 python user_data/ft_client/test_client/test_client.py
         
 #Ref: https://medium.com/@shanejones/how-i-set-up-freqtrade-a287db8966f
