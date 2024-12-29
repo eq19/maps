6ad058ba9b7a6587cea21f924af87df64a6f2736
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-29 09:19:14 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 # Structure: Cell Types – Modulo 6
 
-cd ${GITHUB_WORKSPACE}/user_data/config_examples
-sed -i "s|your_exchange_key|${ACCESS_API}|g" *.json
-sed -i "s|your_exchange_secret|${ACCESS_KEY}|g" *.json
-sed -i "s|your_telegram_chat_id|${MESSAGE_API}|g" *.json
-sed -i "s|your_telegram_token|${MESSAGE_TOKEN}|g" *.json
-
-cd ${GITHUB_WORKSPACE}
 python user_data/ft_client/test_client/test_client.py
         
 #Ref: https://medium.com/@shanejones/how-i-set-up-freqtrade-a287db8966f
