5b8a25d7cf9bcef31437a0d51b1778370c2f8031
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-29 15:47:59 +0700

    Update test_client.py

--- dataFile/user_data/ft_client/test_client/test_client.py
+++ dataFile/user_data/ft_client/test_client/test_client.py
@@ -12,6 +12,19 @@ except Exception as e:
 
 # Test freqtrade
 config = Configuration.from_files(["user_data/config_examples/config_indodax.example.json"])
+print(config)
+
+api_server_config = config.get("api_server")
+if api_server_config:
+    ip_address = api_server_config.get("listen_ip_address")
+    print("IP from config is", ip_address)
+else:
+    print("API server config not found.")
+
+# Retrieve the value of "chat_id" under the "telegram" section
+chat_id = config.get("api_server.listen_ip_address")
+print("API Server from config is", chat_id)
+
 
 # Retrieve the value of "chat_id" under the "telegram" section
 ip_address = config.get("api_server.listen_ip_address")
