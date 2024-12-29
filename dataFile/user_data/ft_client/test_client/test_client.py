ca5a484fa9d244a16c582401fb36259644024152
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-29 14:48:03 +0700

    Update test_client.py

--- dataFile/user_data/ft_client/test_client/test_client.py
+++ dataFile/user_data/ft_client/test_client/test_client.py
@@ -12,7 +12,7 @@ except Exception as e:
 
 # Test freqtrade
 config = Configuration.from_files(["user_data/config_examples/config_indodax.example.json"])
-print("id from config is", config.get("timeframe"))
+print("Communication id from config is", config.get("chat_id"))
 exchange = ExchangeResolver.load_exchange(config)
 ticker = exchange.fetch_ticker('BTC/IDR')
 print(ticker)
