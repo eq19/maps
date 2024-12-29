67d346a92c1174333130a917a12ee26a7a0b950e
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-29 14:59:05 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -4,7 +4,7 @@
 
 hr='------------------------------------------------------------------------------------'
 
-echo -e "\n$hr\nTEST CCXT\n$hr"
+echo -e "\n$hr\nTEST CLIENT\n$hr"
 
 #cd ${{ github.workspace }}/user_data/build_helpers && ./install_ta-lib.sh > /dev/null 2>&1
 python user_data/ft_client/test_client/test_client.py
