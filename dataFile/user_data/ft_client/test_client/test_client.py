207f5c9c4e298e81038b9ce94f7861ca3f5903e9
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-29 23:24:37 +0700

    Update test_client.pybm

--- dataFile/user_data/ft_client/test_client/test_client.py
+++ dataFile/user_data/ft_client/test_client/test_client.py
@@ -1,31 +1,13 @@
 import ccxt
 
-from freqtrade.resolvers import ExchangeResolver
 from freqtrade.configuration import Configuration
+from freqtrade.resolvers import ExchangeResolver
 
 # Test ccxt
 exchange = ccxt.indodax()
-try:
-    print(exchange.fetch_ohlcv('BTC/IDR', timeframe='1m', limit=5))
-except Exception as e:
-    print(f"Error: {e}")
+print(exchange.fetch_ticker('BTC/IDR'))x
 
 # Test freqtrade
 config = Configuration.from_files(["user_data/config_examples/config_indodax.example.json"])
-
-telegram_config = config.get("telegram")
-if telegram_config:
-    telegram_id = telegram_config.get("chat_id")
-    print("Notification ID from config is", telegram_id)
-else:
-    print("Notification ID is not found.")
-
 #exchange = ExchangeResolver.load_exchange(config)
-#print(exchange)
-ticker = exchange.fetch_ticker('BTC/IDR')
-print(ticker)
-
-# Fetch tickers for all pairs
-#tickers = exchange.fetch_tickers()
-#for pair, ticker in tickers.items():
-    #print(f"{pair}: {ticker}")
+print(exchange.fetch_ohlcv('BTC/IDR', timeframe='1m', limit=5))
