e47ab610d3775fddaf6c6e1f2bbd04f952ebfa28
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-30 09:36:27 +0700

    Update test_client.py

--- dataFile/user_data/ft_client/test_client/test_client.py
+++ dataFile/user_data/ft_client/test_client/test_client.py
@@ -3,14 +3,17 @@ import ccxt
 from freqtrade.configuration import Configuration
 from freqtrade.resolvers import ExchangeResolver
 
+config = Configuration.from_files(["user_data/config_examples/config_indodax.example.json"])
+
 # Test ccxt
 exchange = ccxt.indodax()
-print(exchange.fetch_ticker('BTC/IDR'))
 
-# Test freqtrade
-config = Configuration.from_files(["user_data/config_examples/config_indodax.example.json"])
-exchange = ExchangeResolver.load_exchange(config)
-tickers = exchange.fetch_tickers()
-for pair, ticker in tickers.items():
-    print(f"{pair}: {ticker}")
-print(exchange.fetch_ohlcv('BTC/IDR', timeframe='1m', limit=5))
+# Uncomment fot test freqtrade
+# exchange = ExchangeResolver.load_exchange(config)
+
+print("test fetch_ticker", exchange.fetch_ticker('BTC/IDR'))
+print("test fetch_ohlcv", exchange.fetch_ohlcv('BTC/IDR', timeframe='1m', limit=5))
+
+#tickers = exchange.fetch_tickers()
+#for pair, ticker in tickers.items():
+    #print(f"{pair}: {ticker}")
