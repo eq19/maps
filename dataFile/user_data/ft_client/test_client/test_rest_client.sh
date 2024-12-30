b39ebad2d92d945e6863935ec7ad38e62c2c1ad4
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-30 14:17:01 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -12,4 +12,27 @@ python user_data/ft_client/test_client/test_client.py
 echo -e "\n$hr\nTEST NOTIFICATION\n$hr"
 #Ref: https://medium.com/@shanejones/how-i-set-up-freqtrade-a287db8966f
 #freqtrade trade --config user_data/config_examples/config_indodax.example.json
-freqtrade list-pairs --config user_data/config_examples/config_indodax.example.json
+#freqtrade list-pairs --config user_data/config_examples/config_indodax.example.json
+#freqtrade backtesting         Backtesting module.
+#freqtrade backtesting-show    Show past Backtest results
+#freqtrade backtesting-analysis Backtest Analysis module.
+#freqtrade edge                Edge module.
+#freqtrade hyperopt            Hyperopt module.
+#freqtrade hyperopt-list       List Hyperopt results
+#freqtrade hyperopt-show       Show details of Hyperopt results
+#freqtrade list-exchanges      Print available exchanges.
+#freqtrade list-markets        Print markets on exchange.
+#freqtrade list-pairs          Print pairs on exchange.
+#freqtrade list-strategies     Print available strategies.
+#freqtrade list-freqaimodels   Print available freqAI models.
+#freqtrade list-timeframes     Print available timeframes for the exchange.
+#freqtrade show-trades         Show trades.
+#freqtrade test-pairlist       Test your pairlist configuration.
+#freqtrade convert-db          Migrate database to different system
+#freqtrade install-ui          Install FreqUI
+#freqtrade plot-dataframe      Plot candles with indicators.
+#freqtrade plot-profit         Generate plot showing profits.
+#freqtrade webserver           Webserver module.
+#freqtrade strategy-updater    updates outdated strategy files to the current version
+#freqtrade lookahead-analysis  Check for potential look ahead bias.
+#freqtrade recursive-analysis  Check
