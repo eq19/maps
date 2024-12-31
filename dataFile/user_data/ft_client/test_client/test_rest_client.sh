4594b6f43e45c380afd35c899000bb3922ba018c
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-31 15:04:07 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -13,6 +13,11 @@ echo -e "\n$hr\nTEST NOTIFICATION\n$hr"
 #Ref: https://medium.com/@shanejones/how-i-set-up-freqtrade-a287db8966f
 CONFIG=user_data/config_examples/config_indodax.example.json
 
+sed -i "s|your_exchange_key|${ACCESS_API}|g" $CONFIG
+sed -i "s|your_exchange_secret|${ACCESS_KEY}|g" $CONFIG
+sed -i "s|your_telegram_chat_id|${MESSAGE_API}|g" $CONFIG
+sed -i "s|your_telegram_token|${MESSAGE_TOKEN}|g" $CONFIG
+
 freqtrade download-data --config $CONFIG --timeframes 1m 15m
 freqtrade list-data --config $CONFIG
 freqtrade backtesting --config $CONFIG
