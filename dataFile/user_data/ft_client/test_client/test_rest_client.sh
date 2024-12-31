b051980c1928d61a518121bc64c09cfbebefe1f3
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-31 18:34:34 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -9,11 +9,11 @@ pwd && printenv
 
 echo -e "\n$hr\nTEST CLIENT\n$hr"
 #cd ${{ github.workspace }}/user_data/build_helpers && ./install_ta-lib.sh > /dev/null 2>&1
-python user_data/ft_client/test_client/test_client.py
+python /home/runner/user_data/ft_client/test_client/test_client.py
         
 echo -e "\n$hr\nTEST NOTIFICATION\n$hr"
 #Ref: https://medium.com/@shanejones/how-i-set-up-freqtrade-a287db8966f
-CONFIG=user_data/config_examples/config_indodax.example.json
+CONFIG=/home/runner/user_data/config_examples/config_indodax.example.json
 
 sed -i "s|your_exchange_key|${ACCESS_API}|g" $CONFIG
 sed -i "s|your_exchange_secret|${ACCESS_KEY}|g" $CONFIG
