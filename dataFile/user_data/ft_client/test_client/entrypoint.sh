9f28f3cd81c79220d6b367869ffbf5a1624616bf
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-05 23:45:22 +0700

    Update entrypoint.sh

--- dataFile/user_data/ft_client/test_client/entrypoint.sh
+++ dataFile/user_data/ft_client/test_client/entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 # Run in the background
-freqtrade trade &
+if [ -d /mnt/disks/deeplearning ]; then
+  echo "Deeplearning exists. Proceeding...";
+  /mnt/disks/deeplearning/usr/bin/gcloud auth application-default print-access-token > /tmp/token || { echo "Failed to get token"; exit 1; };
+  TOKEN=$(cat /tmp/token)
+  curl -H "Authorization: Bearer $TOKEN" \
+    "https://secretmanager.googleapis.com/v1/projects/feedmapping/secrets/freqtrade-config/versions/latest:access" | \
+    jq -r '.payload.data' | base64 --decode > /home/runner/config.json
+  rm -rf /tmp/*
+  freqtrade trade &
+else
+  echo "Image not found";
+  exit 1;
+fi
 
 # Start PostgreSQL
 docker-entrypoint.sh postgres
