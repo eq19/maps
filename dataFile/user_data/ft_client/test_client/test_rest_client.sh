24b898152659018207488f1e0e64b930f2da0a8f
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-07 22:01:20 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -70,16 +70,17 @@ else
 
   echo -e "\n$hr\nBACKTEST AI TRADES\n$hr"
   freqtrade trade --help
+
   CONFIG=user_data/config_examples/config_indodax.freqai.json
-  #freqtrade trade --config $CONFIG --freqaimodel LightGBMRegressor
-  freqtrade trade --config $CONFIG
+  sed -i "s|your_exchange_key|$ACCESS_API|g" $CONFIG
+  sed -i "s|your_exchange_secret|$ACCESS_KEY|g" $CONFIG
+  sed -i "s|your_telegram_chat_id|$MESSAGE_API|g" $CONFIG
+  sed -i "s|your_telegram_token|$MESSAGE_TOKEN|g" $CONFIG
+
+  freqtrade trade --config $CONFIG --freqaimodel LightGBMRegressor
+  #gh secret set CONFIG_JSON < $CONFIG
+  rm -rf $CONFIG
  
 fi
 
-#sed -i "s|your_exchange_key|$ACCESS_API|g" $CONFIG
-#sed -i "s|your_exchange_secret|$ACCESS_KEY|g" $CONFIG
-#sed -i "s|your_telegram_chat_id|$MESSAGE_API|g" $CONFIG
-#sed -i "s|your_telegram_token|$MESSAGE_TOKEN|g" $CONFIG
 
-#gh secret set CONFIG_JSON < $CONFIG
-#rm -rf $CONFIG
