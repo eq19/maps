64c0bee11a8258923a6f0fbb92b33e5fc82f6b74
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-08 14:19:21 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -48,8 +48,8 @@ else
 
   echo -e "\n$hr\nTEST DOWNLOAD DATA\n$hr"
   freqtrade download-data --help
-  freqtrade download-data --config $CONFIG
-  #freqtrade download-data --config $CONFIG --timeframes 1m 15m 30m 1h
+  #freqtrade download-data --config $CONFIG
+  freqtrade download-data --config $CONFIG --timeframes 1m 15m 30m 1h
 
   echo -e "\n$hr\nLIST DOWNLOAD DATA\n$hr"
   freqtrade list-data --help
