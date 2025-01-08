765ed015fae67c10a48fc11fd25eae2360c13fdf
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-08 13:50:16 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -38,9 +38,9 @@ else
   echo -e "\n$hr\nTEST CLIENT\n$hr"
   python user_data/ft_client/test_client/test_client.py
         
-  echo -e "\n$hr\nSHOW CONFIG\n$hr"
-  freqtrade show-config --help
-  freqtrade show-config --config $CONFIG
+  #echo -e "\n$hr\nSHOW CONFIG\n$hr"
+  #freqtrade show-config --help
+  #freqtrade show-config --config $CONFIG
 
   #echo -e "\n$hr\nTEST PAIR LIST\n$hr"
   #freqtrade test-pairlist --help
@@ -48,8 +48,8 @@ else
 
   echo -e "\n$hr\nTEST DOWNLOAD DATA\n$hr"
   freqtrade download-data --help
-  #freqtrade download-data --config $CONFIG
-  freqtrade download-data --config $CONFIG --timeframes 1m 15m 30m 1h
+  freqtrade download-data --config $CONFIG
+  #freqtrade download-data --config $CONFIG --timeframes 1m 15m 30m 1h
 
   echo -e "\n$hr\nLIST DOWNLOAD DATA\n$hr"
   freqtrade list-data --help
