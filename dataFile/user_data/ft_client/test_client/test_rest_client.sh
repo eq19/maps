276b171cade8f9227a4790276f89f3d2deba0a3d
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-09 05:41:24 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -80,7 +80,8 @@ else
 
   echo -e "\n$hr\nTEST BACKTEST\n$hr"
   freqtrade backtesting --help
-  freqtrade backtesting --config $CONFIG --freqaimodel LightGBMRegressor --timerange="$TB" --export signals
+  freqtrade backtesting --config $CONFIG --timerange="$TB" --export signals
+  #freqtrade backtesting --config $CONFIG --freqaimodel LightGBMRegressor --timerange="$TB" --export signals
 
   #echo -e "\n$hr\nBACKTEST RESULTS\n$hr"
   #ls -alR user_data/backtest_results
