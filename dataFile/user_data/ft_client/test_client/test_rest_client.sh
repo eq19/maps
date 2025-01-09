9bd9a23e07839a68b26a660b065bd07600eb6f12
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-09 23:16:27 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -75,7 +75,7 @@ else
   echo -e "\n$hr\n TEST HYPEROPT\n$hr"
   freqtrade hyperopt --help
   #Ref: https://www.freqtrade.io/en/stable/hyperopt/#solving-a-mystery
-  freqtrade hyperopt --config $CONFIG --fee=$FEE --hyperopt-loss SharpeHyperOptLossDaily -e 500 --spaces all
+  freqtrade hyperopt --config $CONFIG --fee=$FEE --hyperopt-loss SharpeHyperOptLossDaily -e 500
 
   echo -e "\n$hr\nSHOW EDGE\n$hr"
   freqtrade edge --help
