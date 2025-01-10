838fdcc1433a2bb0e5f8cf1c850e37fcc54d13a3
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-10 18:02:41 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -100,10 +100,10 @@ else
   echo -e "\n$hr\nBACKTEST AI TRADES\n$hr"
   freqtrade trade --help
 
-  #sed -i "s|your_exchange_key|$ACCESS_API|g" $CONFIG
-  #sed -i "s|your_exchange_secret|$ACCESS_KEY|g" $CONFIG
-  #sed -i "s|your_telegram_chat_id|$MESSAGE_API|g" $CONFIG
-  #sed -i "s|your_telegram_token|$MESSAGE_TOKEN|g" $CONFIG
+  sed -i "s|your_exchange_key|$ACCESS_API|g" $CONFIG
+  sed -i "s|your_exchange_secret|$ACCESS_KEY|g" $CONFIG
+  sed -i "s|your_telegram_chat_id|$MESSAGE_API|g" $CONFIG
+  sed -i "s|your_telegram_token|$MESSAGE_TOKEN|g" $CONFIG
 
   jq --slurpfile new_pairlists $PAIRFILE '.pairlists = $new_pairlists[0].pairlists' $CONFIG > /home/runner/config.json
   cd /home/runner && freqtrade trade --fee=$FEE #--freqaimodel LightGBMRegressor
