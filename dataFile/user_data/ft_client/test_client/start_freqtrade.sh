5c3f2210fe3606bf1ed3a3dbbf51391da28861fe
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-13 17:27:13 +0700

    Update start_freqtrade.sh

--- dataFile/user_data/ft_client/test_client/start_freqtrade.sh
+++ dataFile/user_data/ft_client/test_client/start_freqtrade.sh
@@ -4,9 +4,9 @@ while true; do
   CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}' | cut -d. -f1)
 
   # Check if CPU utilization is below 50%
-  if [ "$CPU_USAGE" -lt 50 ]; then
-    echo "CPU usage is below 50% ($CPU_USAGE%). Starting Freqtrade..."
-    supervisorctl start freqtrade
+  if [ "$CPU_USAGE" -lt 20 ]; then
+    echo "CPU usage is below 20% ($CPU_USAGE%). Starting Freqtrade..."
+    freqtrade trade &
     break
   else
     echo "CPU usage is too high ($CPU_USAGE%). Retrying in 10 seconds..."
