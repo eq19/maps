63a0bb004acb6922cf346a90de7366685ea24e1e
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-14 12:17:42 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -52,6 +52,16 @@ if [[ "$1" == "listing" ]]; then
   freqtrade list-pairs --help
   freqtrade list-pairs --config $CONFIG
 
+  echo -e "\n$hr\nTEST DOWNLOAD\n$hr"
+  freqtrade download-data --help
+  #freqtrade download-data --config $CONFIG
+  #freqtrade download-data --config $CONFIG --timeframes 1m 15m 30m 1h 1d
+  freqtrade download-data --config $CONFIGS --timeframes 15m 1h 1d 1w --timerange="$TD"
+
+  echo -e "\n$hr\nLIST DOWNLOAD\n$hr"
+  freqtrade list-data --help
+  freqtrade list-data --config $CONFIG
+
   echo -e "\n$hr\nSTRATEGIES\n$hr"
   freqtrade list-strategies --help
   freqtrade list-strategies --config $CONFIG
@@ -66,16 +76,6 @@ else
   echo -e "\n$hr\nTEST CLIENT\n$hr"
   python user_data/ft_client/test_client/test_client.py
         
-  echo -e "\n$hr\nTEST DOWNLOAD\n$hr"
-  freqtrade download-data --help
-  #freqtrade download-data --config $CONFIG
-  #freqtrade download-data --config $CONFIG --timeframes 1m 15m 30m 1h 1d
-  freqtrade download-data --config $CONFIGS --timeframes 15m 1h 1d 1w --timerange="$TD"
-
-  echo -e "\n$hr\nLIST DOWNLOAD\n$hr"
-  #freqtrade list-data --help
-  freqtrade list-data --config $CONFIG
-
   echo -e "\n$hr\nRUN BACKTESTING\n$hr"
   freqtrade backtesting --help
   freqtrade backtesting --config $CONFIGS --fee=$FEE --timerange="$TB" --export signals
@@ -124,4 +124,3 @@ else
   rm -rf *.json
 
 fi
-
