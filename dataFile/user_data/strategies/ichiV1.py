98ece276e9403086ed15e6e59ee9108219784118
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-16 19:41:57 +0700

    Update ichiV1.py

--- dataFile/user_data/strategies/ichiV1.py
+++ dataFile/user_data/strategies/ichiV1.py
@@ -43,9 +43,9 @@ class ichiV1(IStrategy):
     stoploss = -0.275
 
     # Optimal timeframe for the strategy
-    timeframe = '15m'
+    timeframe = '5m'
 
-    startup_candle_count = 32
+    startup_candle_count = 96
     process_only_new_candles = False
 
     trailing_stop = False
@@ -68,14 +68,14 @@ class ichiV1(IStrategy):
             },
             # plot senkou_b, too. Not only the area to it.
             'senkou_b': {},
-            'trend_close_15m': {'color': '#FF5733'},
-            'trend_close_30m': {'color': '#FF8333'},
-            'trend_close_1h': {'color': '#FFB533'},
-            'trend_close_2h': {'color': '#FFE633'},
-            'trend_close_4h': {'color': '#E3FF33'},
-            'trend_close_6h': {'color': '#C4FF33'},
-            'trend_close_8h': {'color': '#61FF33'},
-            'trend_close_1d': {'color': '#33FF7D'}
+            'trend_close_5m': {'color': '#FF5733'},
+            'trend_close_15m': {'color': '#FF8333'},
+            'trend_close_30m': {'color': '#FFB533'},
+            'trend_close_1h': {'color': '#FFE633'},
+            'trend_close_2h': {'color': '#E3FF33'},
+            'trend_close_4h': {'color': '#C4FF33'},
+            'trend_close_6h': {'color': '#61FF33'},
+            'trend_close_8h': {'color': '#33FF7D'}
         },
         'subplots': {
             'fan_magnitude': {
@@ -95,30 +95,28 @@ class ichiV1(IStrategy):
         dataframe['high'] = heikinashi['high']
         dataframe['low'] = heikinashi['low']
 
-        dataframe['trend_close_15m'] = dataframe['close']
-        dataframe['trend_close_30m'] = ta.EMA(dataframe['close'], timeperiod=2)
-        dataframe['trend_close_1h'] = ta.EMA(dataframe['close'], timeperiod=4)
-        dataframe['trend_close_2h'] = ta.EMA(dataframe['close'], timeperiod=8)
-        dataframe['trend_close_4h'] = ta.EMA(dataframe['close'], timeperiod=16)
-        dataframe['trend_close_6h'] = ta.EMA(dataframe['close'], timeperiod=24)
-        dataframe['trend_close_8h'] = ta.EMA(dataframe['close'], timeperiod=32)
-        dataframe['trend_close_1d'] = ta.EMA(dataframe['close'], timeperiod=96)
+        dataframe['trend_close_5m'] = dataframe['close']
+        dataframe['trend_close_15m'] = ta.EMA(dataframe['close'], timeperiod=3)
+        dataframe['trend_close_30m'] = ta.EMA(dataframe['close'], timeperiod=6)
+        dataframe['trend_close_1h'] = ta.EMA(dataframe['close'], timeperiod=12)
+        dataframe['trend_close_2h'] = ta.EMA(dataframe['close'], timeperiod=24)
+        dataframe['trend_close_4h'] = ta.EMA(dataframe['close'], timeperiod=48)
+        dataframe['trend_close_6h'] = ta.EMA(dataframe['close'], timeperiod=72)
+        dataframe['trend_close_8h'] = ta.EMA(dataframe['close'], timeperiod=96)
 
-        dataframe['trend_open_15m'] = dataframe['open']
-        dataframe['trend_open_30m'] = ta.EMA(dataframe['open'], timeperiod=2)
-        dataframe['trend_open_1h'] = ta.EMA(dataframe['open'], timeperiod=4)
-        dataframe['trend_open_2h'] = ta.EMA(dataframe['open'], timeperiod=8)
-        dataframe['trend_open_4h'] = ta.EMA(dataframe['open'], timeperiod=16)
-        dataframe['trend_open_6h'] = ta.EMA(dataframe['open'], timeperiod=24)
-        dataframe['trend_open_8h'] = ta.EMA(dataframe['open'], timeperiod=32)
-        dataframe['trend_open_1d'] = ta.EMA(dataframe['open'], timeperiod=96)
+        dataframe['trend_open_5m'] = dataframe['open']
+        dataframe['trend_open_15m'] = ta.EMA(dataframe['open'], timeperiod=3)
+        dataframe['trend_open_30m'] = ta.EMA(dataframe['open'], timeperiod=6)
+        dataframe['trend_open_1h'] = ta.EMA(dataframe['open'], timeperiod=12)
+        dataframe['trend_open_2h'] = ta.EMA(dataframe['open'], timeperiod=24)
+        dataframe['trend_open_4h'] = ta.EMA(dataframe['open'], timeperiod=48)
+        dataframe['trend_open_6h'] = ta.EMA(dataframe['open'], timeperiod=72)
+        dataframe['trend_open_8h'] = ta.EMA(dataframe['open'], timeperiod=96)
 
         dataframe['fan_magnitude'] = (dataframe['trend_close_1h'] / dataframe['trend_close_8h'])
         dataframe['fan_magnitude_gain'] = dataframe['fan_magnitude'] / dataframe['fan_magnitude'].shift(1)
 
-        #ichimoku = ftt.ichimoku(dataframe, conversion_line_period=20, base_line_periods=60, laggin_span=120, displacement=30)
-        ichimoku = ftt.ichimoku(dataframe, conversion_line_period=7, base_line_periods=20, lagging_span=40, displacement=10)
-        
+        ichimoku = ftt.ichimoku(dataframe, conversion_line_period=20, base_line_periods=60, laggin_span=120, displacement=30)
         dataframe['chikou_span'] = ichimoku['chikou_span']
         dataframe['tenkan_sen'] = ichimoku['tenkan_sen']
         dataframe['kijun_sen'] = ichimoku['kijun_sen']
@@ -140,63 +138,63 @@ class ichiV1(IStrategy):
 
         # Trending market
         if self.buy_params['buy_trend_above_senkou_level'] >= 1:
+            conditions.append(dataframe['trend_close_5m'] > dataframe['senkou_a'])
+            conditions.append(dataframe['trend_close_5m'] > dataframe['senkou_b'])
+
+        if self.buy_params['buy_trend_above_senkou_level'] >= 2:
             conditions.append(dataframe['trend_close_15m'] > dataframe['senkou_a'])
             conditions.append(dataframe['trend_close_15m'] > dataframe['senkou_b'])
 
-        if self.buy_params['buy_trend_above_senkou_level'] >= 2:
+        if self.buy_params['buy_trend_above_senkou_level'] >= 3:
             conditions.append(dataframe['trend_close_30m'] > dataframe['senkou_a'])
             conditions.append(dataframe['trend_close_30m'] > dataframe['senkou_b'])
 
-        if self.buy_params['buy_trend_above_senkou_level'] >= 3:
+        if self.buy_params['buy_trend_above_senkou_level'] >= 4:
             conditions.append(dataframe['trend_close_1h'] > dataframe['senkou_a'])
             conditions.append(dataframe['trend_close_1h'] > dataframe['senkou_b'])
 
-        if self.buy_params['buy_trend_above_senkou_level'] >= 4:
+        if self.buy_params['buy_trend_above_senkou_level'] >= 5:
             conditions.append(dataframe['trend_close_2h'] > dataframe['senkou_a'])
             conditions.append(dataframe['trend_close_2h'] > dataframe['senkou_b'])
 
-        if self.buy_params['buy_trend_above_senkou_level'] >= 5:
+        if self.buy_params['buy_trend_above_senkou_level'] >= 6:
             conditions.append(dataframe['trend_close_4h'] > dataframe['senkou_a'])
             conditions.append(dataframe['trend_close_4h'] > dataframe['senkou_b'])
 
-        if self.buy_params['buy_trend_above_senkou_level'] >= 6:
+        if self.buy_params['buy_trend_above_senkou_level'] >= 7:
             conditions.append(dataframe['trend_close_6h'] > dataframe['senkou_a'])
             conditions.append(dataframe['trend_close_6h'] > dataframe['senkou_b'])
 
-        if self.buy_params['buy_trend_above_senkou_level'] >= 7:
+        if self.buy_params['buy_trend_above_senkou_level'] >= 8:
             conditions.append(dataframe['trend_close_8h'] > dataframe['senkou_a'])
             conditions.append(dataframe['trend_close_8h'] > dataframe['senkou_b'])
 
-        if self.buy_params['buy_trend_above_senkou_level'] >= 8:
-            conditions.append(dataframe['trend_close_1d'] > dataframe['senkou_a'])
-            conditions.append(dataframe['trend_close_1d'] > dataframe['senkou_b'])
-
         # Trends bullish
         if self.buy_params['buy_trend_bullish_level'] >= 1:
-            conditions.append(dataframe['trend_close_15m'] > dataframe['trend_open_5m'])
+            conditions.append(dataframe['trend_close_5m'] > dataframe['trend_open_5m'])
 
         if self.buy_params['buy_trend_bullish_level'] >= 2:
-            conditions.append(dataframe['trend_close_30m'] > dataframe['trend_open_30m'])
+            conditions.append(dataframe['trend_close_15m'] > dataframe['trend_open_15m'])
 
         if self.buy_params['buy_trend_bullish_level'] >= 3:
-            conditions.append(dataframe['trend_close_1h'] > dataframe['trend_open_1h'])
+            conditions.append(dataframe['trend_close_30m'] > dataframe['trend_open_30m'])
 
         if self.buy_params['buy_trend_bullish_level'] >= 4:
-            conditions.append(dataframe['trend_close_2h'] > dataframe['trend_open_2h'])
+            conditions.append(dataframe['trend_close_1h'] > dataframe['trend_open_1h'])
 
         if self.buy_params['buy_trend_bullish_level'] >= 5:
-            conditions.append(dataframe['trend_close_4h'] > dataframe['trend_open_4h'])
+            conditions.append(dataframe['trend_close_2h'] > dataframe['trend_open_2h'])
 
         if self.buy_params['buy_trend_bullish_level'] >= 6:
-            conditions.append(dataframe['trend_close_6h'] > dataframe['trend_open_6h'])
+            conditions.append(dataframe['trend_close_4h'] > dataframe['trend_open_4h'])
 
         if self.buy_params['buy_trend_bullish_level'] >= 7:
-            conditions.append(dataframe['trend_close_8h'] > dataframe['trend_open_8h'])
+            conditions.append(dataframe['trend_close_6h'] > dataframe['trend_open_6h'])
 
         if self.buy_params['buy_trend_bullish_level'] >= 8:
-            conditions.append(dataframe['trend_close_1d'] > dataframe['trend_open_1d'])
+            conditions.append(dataframe['trend_close_8h'] > dataframe['trend_open_8h'])
 
-# Trends magnitude
+        # Trends magnitude
         conditions.append(dataframe['fan_magnitude_gain'] >= self.buy_params['buy_min_fan_magnitude_gain'])
         conditions.append(dataframe['fan_magnitude'] > 1)
 
@@ -215,7 +213,7 @@ class ichiV1(IStrategy):
 
         conditions = []
 
-        conditions.append(qtpylib.crossed_below(dataframe['trend_close_15m'], dataframe[self.sell_params['sell_trend_indicator']]))
+        conditions.append(qtpylib.crossed_below(dataframe['trend_close_5m'], dataframe[self.sell_params['sell_trend_indicator']]))
 
         if conditions:
             dataframe.loc[
