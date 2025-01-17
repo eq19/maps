a380f812d74119e43a8ea97376d73598caae415b
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-17 10:20:17 +0700

    Update ichiV1.py

--- dataFile/user_data/strategies/ichiV1.py
+++ dataFile/user_data/strategies/ichiV1.py
@@ -112,8 +112,13 @@ class ichiV1(IStrategy):
         dataframe['fan_magnitude'] = (dataframe['trend_close_1h'] / dataframe['trend_close_8h'])
         dataframe['fan_magnitude_gain'] = dataframe['fan_magnitude'] / dataframe['fan_magnitude'].shift(1)
 
-        #ichimoku = ftt.ichimoku(dataframe, conversion_line_period=20, base_line_periods=60, laggin_span=120, displacement=30)
-        ichimoku = ftt.ichimoku(dataframe, conversion_line_period=7, base_line_periods=20, laggin_span=40, displacement=10)
+        displacement = 30
+        ichimoku = ftt.ichimoku(dataframe, 
+            conversion_line_period=20, 
+            base_line_periods=60,
+            laggin_span=120, 
+            displacement=displacement
+            )
         
         dataframe['chikou_span'] = ichimoku['chikou_span']
         dataframe['tenkan_sen'] = ichimoku['tenkan_sen']
