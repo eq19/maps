783fd3ae4b863d4436d65e42039f021b68d2141e
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-19 11:49:07 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -120,7 +120,7 @@ else
 
   echo "Starting freqtrade trade..."
   #freqtrade trade --freqaimodel LightGBMRegressor
-  nohup freqtrade trade --dry-run --fee=$FEE --config $CONFIG > freqtrade.log 2>&1 &
+  nohup freqtrade trade --dry-run --fee=$FEE > freqtrade.log 2>&1 &
   echo $! > freqtrade_pid.txt
   tail -f freqtrade.log | while read LOGLINE
   do
