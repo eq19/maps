614acc8b56b082cf9516030af1270fc0c954a843
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-20 08:01:19 +0700

    Update ichiV1.py

--- dataFile/user_data/strategies/ichiV1.py
+++ dataFile/user_data/strategies/ichiV1.py
@@ -151,26 +151,6 @@ class ichiV1(IStrategy):
     #trailing_stop_positive_offset = 0.025
     #trailing_only_offset_is_reached = True
 
-    # Buy hyperspace params:
-    buy_params = {
-        "buy_trend_above_senkou_level": 1,
-        "buy_trend_bullish_level": 6,
-        "buy_fan_magnitude_shift_value": 3,
-        "buy_min_fan_magnitude_gain": 1.002 # NOTE: Good value (Win% ~70%), alot of trades
-        #"buy_min_fan_magnitude_gain": 1.008 # NOTE: Very save value (Win% ~90%), only the biggest moves 1.008,
-    }
-
-    # Sell hyperspace params:
-    # NOTE: was 15m but kept bailing out in dryrun
-    sell_params = {
-        "pHSL": -0.08,
-        "ProfitLoss1": 0.005,
-        "ProfitLoss2": 0.021,
-        "ProfitMargin1": 0.018,
-        "ProfitMargin2": 0.051,
-        "sell_trend_indicator": "trend_close_2h"
-    }
-
     plot_config = {
         'main_plot': {
             # fill area between senkou_a and senkou_b
@@ -201,12 +181,33 @@ class ichiV1(IStrategy):
         }
     }
 
+    # Buy hyperspace params:
+    buy_params = {
+        "buy_trend_above_senkou_level": 1,
+        "buy_trend_bullish_level": 6,
+        "buy_fan_magnitude_shift_value": 3,
+        "buy_min_fan_magnitude_gain": 1.002 # NOTE: Good value (Win% ~70%), alot of trades
+        #"buy_min_fan_magnitude_gain": 1.008 # NOTE: Very save value (Win% ~90%), only the biggest moves 1.008,
+    }
+
+    # Sell hyperspace params:
+    # NOTE: was 15m but kept bailing out in dryrun
+    sell_params = {
+        "pHSL": -0.08,
+        "ProfitLoss1": 0.005,
+        "ProfitLoss2": 0.021,
+        "ProfitMargin1": 0.018,
+        "ProfitMargin2": 0.051,
+        "ExitTrendIndicator": "trend_close_2h"
+    }
+
     # trailing stoploss hyperopt parameters
     pHSL = DecimalParameter(-0.15, -0.08, default=sell_params['pHSL'], decimals=3, space='sell', optimize=True)
-    ProfitMargin1 = DecimalParameter(0.009, 0.019, default=sell_params['ProfitMargin1'], decimals=3, space='sell', optimize=True)
     ProfitLoss1 = DecimalParameter(0.005, 0.012, default=sell_params['ProfitLoss1'], decimals=3, space='sell', optimize=True)
-    ProfitMargin2 = DecimalParameter(0.033, 0.099, default=sell_params['ProfitMargin2'], decimals=3, space='sell', optimize=True)
     ProfitLoss2 = DecimalParameter(0.010, 0.025, default=sell_params['ProfitLoss2'], decimals=3, space='sell', optimize=True)
+    ProfitMargin1 = DecimalParameter(0.009, 0.019, default=sell_params['ProfitMargin1'], decimals=3, space='sell', optimize=True)
+    ProfitMargin2 = DecimalParameter(0.033, 0.099, default=sell_params['ProfitMargin2'], decimals=3, space='sell', optimize=True)
+    ExitTrendIndicator = CategoricalParameter(['trend_close_30m', 'trend_close_1h', 'trend_close_2h', 'trend_close_4h', 'trend_close_6h', 'trend_close_8h'], default='trend_close_2h', space='sell')
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
