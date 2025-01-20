c127ae6e7816fc61b22eaa2ebd6df34e7b2b0ff9
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-20 09:00:55 +0700

    Update freqtrade.sh

--- dataFile/user_data/ft_client/test_client/freqtrade.sh
+++ dataFile/user_data/ft_client/test_client/freqtrade.sh
@@ -9,13 +9,20 @@ TELEGRAM_CHAT_ID=$(jq -r '.telegram.chat_id' "$CONFIG_FILE")
 
 # Function to send a message to Telegram
 send_telegram_message() {
-    local message=$1
+    local log_type=$1
+    local message=$2
     curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
     -d chat_id="${TELEGRAM_CHAT_ID}" \
-    -d text="${message}" > /dev/null
+    -d text="[$log_type] $message" > /dev/null
 }
 
-# Monitor the log file and send updates to Telegram
-tail -f /var/log/freqtrade.out.log /var/log/freqtrade.error.log | while read line; do
-    send_telegram_message "$line"
+# Monitor the log files and send updates to Telegram
+tail -f /var/log/freqtrade.out.log | while read line; do
+    send_telegram_message "OUT" "$line"
+done &
+
+tail -f /var/log/freqtrade.error.log | while read line; do
+    send_telegram_message "ERROR" "$line"
 done
+
+wait
