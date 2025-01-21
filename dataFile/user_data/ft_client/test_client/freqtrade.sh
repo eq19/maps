7e8269ab1757f3f3472ce87058ec62b7a2cc3f2d
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-21 16:00:29 +0700

    Update freqtrade.sh

--- dataFile/user_data/ft_client/test_client/freqtrade.sh
+++ dataFile/user_data/ft_client/test_client/freqtrade.sh
@@ -4,6 +4,7 @@
 send_telegram_message() {
     local log_type=$1
     local message=$2
+    # https://www.freqtrade.io/en/stable/faq/#how-do-i-search-the-bot-logs-for-something
     curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
     -d chat_id="${TELEGRAM_CHAT_ID}" \
     -d text="[$log_type] $message" > /dev/null
