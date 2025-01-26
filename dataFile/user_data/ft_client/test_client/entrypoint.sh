adec8c7961a0f1b72788ab1df88fefa9cacba01d
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-27 06:06:39 +0700

    Update entrypoint.sh

--- dataFile/user_data/ft_client/test_client/entrypoint.sh
+++ dataFile/user_data/ft_client/test_client/entrypoint.sh
@@ -4,11 +4,11 @@
 if [ -d /mnt/disks/deeplearning ]; then
   /mnt/disks/deeplearning/usr/bin/gcloud auth application-default print-access-token > /tmp/token || { echo "Failed to get token"; exit 1; };
   TOKEN=$(cat /tmp/token)
-  curl -H "Authorization: Bearer $TOKEN" \
-    "https://secretmanager.googleapis.com/v1/projects/feedmapping/secrets/freqtrade-config/versions/latest:access" | \
-    jq -r '.payload.data' | base64 --decode > /home/runner/config.json
+  #curl -H "Authorization: Bearer $TOKEN" \
+    #"https://secretmanager.googleapis.com/v1/projects/feedmapping/secrets/freqtrade-config/versions/latest:access" | \
+    #jq -r '.payload.data' | base64 --decode > /home/runner/config.json
 
-  # Run PostgreSQL (autostart) and Freqtrade (cpu low)
+  # Run PostgreSQL (autostart)
   #sudo service supervisor start
   exec supervisord -c /etc/supervisor/supervisord.conf
 else
