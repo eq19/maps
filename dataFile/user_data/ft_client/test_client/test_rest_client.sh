5e3f647d79b946581dd3d204bf61e8e046e77941
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-27 20:44:16 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -96,7 +96,7 @@ else
   #Ref: https://www.freqtrade.io/en/stable/hyperopt/#solving-a-mystery
   #sed -i "s|if params.get(FTHYPT_FILEVERSION, 1) >= 2 and not config.get(\"disableparamexport\", False):|logger.warning(f'{params.get(FTHYPT_FILEVERSION, 1)} and {config.get(\"disableparamexport\", False)}')|g" $HYPERPY
   #sed -i "s|# Export parameters ...|logger.warning(f'{HyperoptTools.get_strategy_filename(config, strategy_name)}')|g" $HYPERPY && cat $HYPERPY
-  freqtrade hyperopt --config $CONFIG -e 10 --fee=$FEE --spaces buy sell roi
+  freqtrade hyperopt --config $CONFIG -e 10 --fee=$FEE --spaces roi buy sell trailing
 
   echo -e "\n$hr\nRERUN BACKTEST\n$hr"
   cat user_data/strategies/$STRATEGY.json
