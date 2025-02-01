43b29da71b76bcce9d44b1b727dbc0ffbe08877f
eQuantum <8466209+eq19@users.noreply.github.com>
2025-02-01 11:08:03 +0700

    Update ichiV1.py

--- dataFile/user_data/strategies/ichiV1.py
+++ dataFile/user_data/strategies/ichiV1.py
@@ -215,11 +215,45 @@ class ichiV1(IStrategy):
                  side: str, **kwargs) -> float:
         return 1.0  # Conservative leverage
 
+    # Protection Space  
     cooldown_period = IntParameter(1, 20, default=7, space="protection")
     lookback_period = IntParameter(5, 50, default=24, space="protection")
     trade_limit = IntParameter(1, 10, default=4, space="protection")
     stop_duration = IntParameter(1, 24, default=12, space="protection")
+    cooldown_lookback = IntParameter(2, 90, default=3, space="protection", optimize=True)
+    stop_duration = IntParameter(12, 100, default=3, space="protection", optimize=True)
+    use_stop_protection = BooleanParameter(default=True, space="protection", optimize=True)
 
+    @property
+    def protections(self):
+        return [
+            {
+                "method": "CooldownPeriod",
+                "stop_duration_candles": self.cooldown_lookback.value
+            },
+            {
+                "method": "MaxDrawdown",
+                "lookback_period_candles": self.cooldown_lookback.value,
+                "trade_limit": 5,
+                "stop_duration_candles": self.stop_duration.value,
+                "max_allowed_drawdown": 0.9
+            },
+            {
+                "method": "StoplossGuard",
+                "lookback_period_candles": 20,
+                "trade_limit": 3,
+                "stop_duration_candles": 4,
+                "only_per_pair": False
+            },
+            {
+                "method": "LowProfitPairs",
+                "lookback_period_candles": 24,
+                "trade_limit": 2,
+                "stop_duration_candles": 4,
+                "required_profit": 0.01
+            }
+        ]
+  
     @property
     def protections(self):
         return [
