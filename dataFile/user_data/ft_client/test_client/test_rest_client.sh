285a1166b312e976c00c13c4900faf6193d36ce4
eQuantum <8466209+eq19@users.noreply.github.com>
2025-02-11 23:17:22 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -80,8 +80,8 @@ else
   echo -e "\n$hr\nRUN BACKTESTING\n$hr"
   freqtrade backtesting --help
   freqtrade backtesting --fee=$FEE --timerange="$TB" --enable-protections --export trades
+  unzip $(ls -t /home/runner/user_data/backtest_results/backtest-result-*.zip | head -n 1)
   ls -al /home/runner/user_data/backtest_results
-  jq . $(ls -t /home/runner/user_data/backtest_results/backtest-result-*.json | head -n 1)
 
   echo -e "\n$hr\nRUN HYPEROPT\n$hr"
   freqtrade hyperopt --help
