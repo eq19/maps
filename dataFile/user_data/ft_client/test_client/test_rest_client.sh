4d5401d79b93ea6e30cae0317f4c2ee9fa9d5b5d
eQuantum <8466209+eq19@users.noreply.github.com>
2025-02-12 08:38:54 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -84,7 +84,7 @@ else
   cd /home/runner/user_data/backtest_results
   unzip $(ls -t backtest-result-*.zip | head -n 1) && ls -al .
   LATEST_JSON=$(ls -t backtest-result-*.json | head -n 1)
-  echo $(jq -r '.strategy_comparison[]."winrate"' $LATEST_JSON)
+  echo $(jq '.strategy_comparison[] | select(.key == "fibbo") | .winrate' $LATEST_JSON)
   rm -rf * && cd /home/runner
 
   echo -e "\n$hr\nRUN HYPEROPT\n$hr"
@@ -120,7 +120,7 @@ else
   cd /home/runner/user_data/backtest_results
   unzip $(ls -t backtest-result-*.zip | head -n 1) && ls -al .
   LATEST_JSON=$(ls -t backtest-result-*.json | head -n 1)
-  echo $(jq -r '.strategy_comparison[]."winrate"' $LATEST_JSON)
+  echo $(jq '.strategy_comparison[] | select(.key == "fibbo") | .winrate' $LATEST_JSON)
   rm -rf * && cd /home/runner
 
   #echo -e "\n$hr\nANALYSIS\n$hr"
