fffedc89689f54d1d657ac469e1e77f8fad4f1d5
eQuantum <8466209+eq19@users.noreply.github.com>
2025-02-14 08:07:50 +0700

    Update freqtrade.sh

--- dataFile/user_data/ft_client/test_client/freqtrade.sh
+++ dataFile/user_data/ft_client/test_client/freqtrade.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# Function to send a message to Telegram
+# Define the function to send a Telegram message
 send_telegram_message() {
-    local message=$1
+    local message="$1"
+    local bot_token="$WARNING_BOT_TOKEN"
+    local chat_id="$TELEGRAM_CHAT_ID"
+    
     # https://www.freqtrade.io/en/stable/faq/#how-do-i-search-the-bot-logs-for-something
-    curl -s -X POST "https://api.telegram.org/bot${WARNING_BOT_TOKEN}/sendMessage" -d chat_id="${TELEGRAM_CHAT_ID}" -d text="$message" > /dev/null
+    curl -s -X POST "https://api.telegram.org/bot$bot_token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d text="$message" > /dev/null
 }
 
-# Monitor the log files and send updates to Telegram
-# https://www.freqtrade.io/en/2023.11/advanced-setup/#logging-to-syslog
-tail -f /mnt/disks/deeplearning/var/log/apt/freqtrade.log | grep --line-buffered -iE "WARNING|ERROR" | while read line; do
+# Run the log monitoring command in the background
+tail -f /mnt/disks/deeplearning/var/log/apt/freqtrade.log | \
+grep --line-buffered -iE "WARNING|ERROR" | \
+while read -r line; do
     send_telegram_message "$line"
 done &
 
