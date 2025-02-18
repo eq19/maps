d60a79e53085d492d2cb731d36ed80d3986d6fbc
eQuantum <8466209+eq19@users.noreply.github.com>
2025-02-18 11:55:10 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -62,7 +62,7 @@ hyperopt() {
       --logfile /dev/null > /dev/null 2>&1
 
     echo -e "\n$hr\nStep-$id: Hyperopt Result\n$hr"
-    freqtrade hyperopt-list
+    freqtrade hyperopt-list --best --no-details
     echo -e "\n$hr\nStep-$id: Backtesting Results\n$hr"
     freqtrade hyperopt-show --best
   done
