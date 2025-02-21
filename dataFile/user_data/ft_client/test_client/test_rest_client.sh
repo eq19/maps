ed605e860233b4960ac857a93fafc7e04af9f301
eQuantum <8466209+eq19@users.noreply.github.com>
2025-02-21 07:47:31 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -15,14 +15,14 @@ PAIRFILE=user_data/config_examples/config_pairlist.example.json
 HYPERPY=/home/runner/venv/lib/python3.11/site-packages/freqtrade/optimize/hyperopt_tools.py
 
 # Define the backtesting duration (in days)
-BACKTESTING_DURATION=12  # Adjust as per your strategy
+BACKTESTING_DURATION=6  # Adjust as per your strategy
 
 # Today's date in the required format (YYYYMMDD)
 TODAY=$(date -u +%Y%m%d)
 YESTERDAY=$(date -u -d "yesterday" +%Y%m%d)
 
 # 30 days ago in the required format
-EARLIEST_DATE=$(date -u -d "30 days ago" +%Y%m%d)
+EARLIEST_DATE=$(date -u -d "9 days ago" +%Y%m%d)
 
 # Backtesting start date in the required format (earliest_date + sliding window)
 BACKTESTING_START=$(date -u -d "$EARLIEST_DATE + $BACKTESTING_DURATION days" +%Y%m%d)
