12f84e72bfece78aa2498d1c81b69f5a174c4885
eq19 <eq19@users.noreply.github.com>
2025-03-05 07:01:21 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -18,7 +18,10 @@ TARGET_REPO="https://${GITHUB_ACTOR}:${GH_TOKEN}@github.com/${TARGET_REPOSITORY}
 REMOTE_REPO="https://${GITHUB_ACTOR}:${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 API_URL="https://api.github.com/users/eq19/events/public"
-LATEST_COMMIT=$(curl -s $API_URL | jq -r 'map(select(.type == "PushEvent")) | .[0].payload.commits[0].message')
+COMMIT=$(curl -s $API_URL | jq -r 'map(select(.type == "PushEvent")) | .[0].payload.commits[0].message')
+
+# Remove double quotes using parameter expansion
+LATEST_COMMIT="${COMMIT//\"/}"
 
 if [[ -z "$LATEST_COMMIT" ]] || [[ "$LATEST_COMMIT" == "null" ]]; then
   echo 'LATEST_COMMIT="update by workspace"' >> ${GITHUB_ENV}
@@ -45,8 +48,10 @@ fi
 echo -e "\n$hr\nWORKSPACE\n$hr"
 if [[ "${JOB_ID}" == "1" ]]; then
 
+  #[[ "${LATEST_COMMIT}" == *"RERUN_RUNNER"* ]] && gh variable set RERUN_RUNNER --body "true"
+
   cd ${GITHUB_WORKSPACE} && rm -rf .github
-  cp -r /home/runner/work/_actions/eq19/eq19/v1/.github .
+  cp -r /home/runner/work/_actions/eq19/eq19/v2/.github .
   chown -R "$(whoami)" .github
 
   git remote set-url origin ${REMOTE_REPO}        
@@ -56,6 +61,9 @@ if [[ "${JOB_ID}" == "1" ]]; then
     git add . && git commit --allow-empty -m "rerun due to job update" && git push
     exit 1
   else
+    PARAMS=.github/entrypoint/artifact/python/src/params/spaces.json
+    mv -f ${GITHUB_WORKSPACE}/$PARAMS $1/dataFile/user_data/strategies/fibbo.json
+  
     mv -f $1/dataFile/user_data ${GITHUB_WORKSPACE}/
     cd ${GITHUB_WORKSPACE} && ls -al ${GITHUB_WORKSPACE}
   fi
@@ -66,16 +74,14 @@ elif [[ "${JOB_ID}" == "2" ]]; then
 
 elif [[ "${JOB_ID}" == "3" ]]; then
 
-  cat /home/runner/_site/_config.yml
-  gist.sh ${TARGET_REPOSITORY} ${FOLDER}
+  cd /home/runner/_site && cat _config.yml
+  gist.sh ${BASE} > /dev/null && cat ${RUNNER_TEMP}/spin.txt
+  cp -R ${RUNNER_TEMP}/wikidir/* .
 
-  find ${RUNNER_TEMP}/gistdir -type d -name .git -prune -exec rm -rf {} \;
-  mv -f ${RUNNER_TEMP}/workdir/* /home/runner/_site/
-
-  rm -rf ${RUNNER_TEMP}/Sidebar.md && cp _Sidebar.md ${RUNNER_TEMP}/Sidebar.md
-  sed -i 's/0. \[\[//g' ${RUNNER_TEMP}/Sidebar.md && sed -i 's/\]\]//g' ${RUNNER_TEMP}/Sidebar.md
-
-  cd /home/runner/_site && cp -R ${RUNNER_TEMP}/gistdir/* . && ls -lR .
+  if [[ "${WIKI}" != "${BASE}" ]]; then
+    gist.sh ${WIKI} > /dev/null && cat ${RUNNER_TEMP}/spin.txt
+    find . -type d -name "${FOLDER}" -prune -exec sh -c 'cp -R ${RUNNER_TEMP}/wikidir/* "$1/"' sh {} \;
+  fi
 
 else
 
@@ -83,7 +89,6 @@ else
   git clone --single-branch --branch gh-source $TARGET_REPO gh-source
   
   cd ${GITHUB_WORKSPACE//\\//}
-  #find -not -path "./.git/*" -not -name ".git" | grep git
   find -not -path "./.git/*" -not -name ".git" -delete
 
   rm -rf ${RUNNER_TEMP//\\//}/gh-source/.git
