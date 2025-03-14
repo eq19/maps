6e4ca2b759aa5a2699a4bfff0701c420e986bb2a
eQuantum <8466209+eq19@users.noreply.github.com>
2025-03-14 10:15:41 +0700

    Update test_rest_client.sh

--- dataFile/user_data/ft_client/test_client/test_rest_client.sh
+++ dataFile/user_data/ft_client/test_client/test_rest_client.sh
@@ -250,8 +250,8 @@ else
   git config --global user.email eq19@users.noreply.github.com
   git clone https://eq19:$TOKEN@github.com/$TARGET_REPOSITORY.wiki.git /tmp/wiki
 
-  cd /tmp/wiki && mv -f /home/runner/user_data .
-  rm -rf /home/runner/user_data/build_helpers
+  cd /tmp/wiki && mv -f /home/runner/user_data . && rm -rf user_data/build_helpers
+  find user_data/strategies -mindepth 1 -type d -exec rm -rf {} +
   git add . && git commit --allow-empty -m "update params" && git push
   
 fi
