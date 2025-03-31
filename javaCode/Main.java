c2d5066a0f993d9c03f5ade5c5ee6e6b848d31de
eQuantum <8466209+eq19@users.noreply.github.com>
2025-03-31 10:05:00 +0700

    Update Main.java

--- javaCode/Main.java
+++ javaCode/Main.java
@@ -1,11 +1,13 @@
 public abstract class Main {
+    private static String projectName = "eQuantum";
+    
     public static void foo(int x) {  }
     public static void foo(float x) {  }
 
     public static void main(String[] args) {
         System.out.println("************************************");
         System.out.println("  Prime Hexagon Near Pi");
-        System.out.println("  by eQuantum Project");
+        System.out.println("  by " + projectName + " Project");
         System.out.println("************************************");
         System.out.println("");
     }
