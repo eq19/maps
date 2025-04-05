6ed4bd6e4b6b41c48bdc1ee1fcad520d57b20711
eQuantum <8466209+eq19@users.noreply.github.com>
2025-04-05 10:56:07 +0700

    Update test_rest_client.sh

--- user_data/ft_client/test_client/test_rest_client.sh
+++ user_data/ft_client/test_client/test_rest_client.sh
@@ -199,7 +199,7 @@ else
       -H "Accept: application/vnd.github+json" \
       -H "Authorization: Bearer $GH_TOKEN" \
       -H "X-GitHub-Api-Version: 2022-11-28" \
-      "https://api.github.com/repos/OWNER/REPO/actions/variables/PARAMS_JSON" \
+      "https://api.github.com/repos/$TARGET_REPOSITORY/actions/variables/PARAMS_JSON" \
       -d "{\"name\":\"PARAMS_JSON\",\"value\":\"$(jq -r '.' $STRATEGY)\"}"
   fi
 
