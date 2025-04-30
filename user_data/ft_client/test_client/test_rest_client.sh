95413f96456a430bcb413f7f5906e68d9ba07e8c
eQuantum <8466209+eq19@users.noreply.github.com>
2025-04-30 09:42:52 +0700

    Update test_rest_client.sh

--- user_data/ft_client/test_client/test_rest_client.sh
+++ user_data/ft_client/test_client/test_rest_client.sh
@@ -205,6 +205,20 @@ else
   freqtrade hyperopt --help && freqtrade list-hyperoptloss
   python user_data/ft_client/test_client/app.py output.txt
   cat user_data/ft_client/test_client/results/output.txt
+ 
+  curl -L -s -X PATCH \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $GH_TOKEN" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/$TARGET_REPOSITORY/actions/variables/PARAMS_JSON \
+    -d "$(jq -n '{name:"PARAMS_JSON", value:$value}' --arg value "$(cat "$STRATEGY")")"
+
+  curl -s -X POST \
+    -H "Authorization: Bearer ${BEARER}" \
+    -H "Content-Type: application/json" \
+    https://us-central1-feedmapping.cloudfunctions.net/function \
+    --data @${STRATEGY} | jq '.'
+
   hyperopt $ID
 
   #echo -e "\n$hr\nANALYSIS\n$hr"
