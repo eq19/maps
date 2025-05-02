cd03ec87b2fd5cf233edd3eb9895059c090972a7
eQuantum <8466209+eq19@users.noreply.github.com>
2025-05-02 17:21:08 +0700

    Update test_client.sh

--- user_data/ft_client/test_client/test_client.sh
+++ user_data/ft_client/test_client/test_client.sh
@@ -2,11 +2,11 @@
 #
 ls -al $WORKSPACE
 
-STRATEGY=$WORKSPACE/user_data/ft_client/test_client/results/output.txt
-cat $STRATEGY
+cat $WORKSPACE/user_data/ft_client/test_client/results/output.txt
+ARTIFACT=$WORKSPACE/user_data/ft_client/test_client/results/orgs.jsom
 
 curl -s -X POST \
   -H "Authorization: Bearer ${BEARER}" \
   -H "Content-Type: application/json" \
   https://us-central1-feedmapping.cloudfunctions.net/function \
-  --data @${STRATEGY} | jq '.'
+  --data @${ARTIFACT} | jq '.'
