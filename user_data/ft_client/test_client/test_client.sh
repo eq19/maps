5775221f7458b7ae71c41ab2e1fc61fd3a5a6f3f
eQuantum <8466209+eq19@users.noreply.github.com>
2025-05-02 14:49:06 +0700

    Update test_client.sh

--- user_data/ft_client/test_client/test_client.sh
+++ user_data/ft_client/test_client/test_client.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 #
 ls -al $WORKSPACE
+
+cat $WORKSPACE/user_data/ft_client/test_client/results/output.txt
+
+curl -s -X POST \
+  -H "Authorization: Bearer ${BEARER}" \
+  -H "Content-Type: application/json" \
+  https://us-central1-feedmapping.cloudfunctions.net/function \
+  --data @${STRATEGY} | jq '.'
+
+
+  
