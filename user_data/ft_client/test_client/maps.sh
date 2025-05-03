680dc94f84c3bf6fae81166ffc67b3733410440e
eq19 <eq19@users.noreply.github.com>
2025-05-03 23:15:13 +0000

    update script

---
+++ user_data/ft_client/test_client/maps.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+ls -al $WORKSPACE
+
+cat $WORKSPACE/user_data/ft_client/test_client/results/output.txt
+ARTIFACT=$WORKSPACE/user_data/ft_client/test_client/results/orgs.json
+
+curl -s -X POST \
+  -H "Authorization: Bearer ${BEARER}" \
+  -H "Content-Type: application/json" \
+  https://us-central1-feedmapping.cloudfunctions.net/function \
+  --data @${ARTIFACT} | jq '.'
+
+curl -L -s -X PATCH \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GH_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/ORGS_JSON \
+  -d "$(jq -n '{name:"ORGS_JSON", value:$value}' --arg value "$(cat "$ARTIFACT")")"
+        
