5b391890471fe8c88787c8f3580d99de7be2ff7e
eQuantum <8466209+eq19@users.noreply.github.com>
2025-05-04 08:46:06 +0700

    Update maps.sh

--- user_data/ft_client/test_client/maps.sh
+++ user_data/ft_client/test_client/maps.sh
@@ -9,11 +9,3 @@ curl -s -X POST \
   -H "Content-Type: application/json" \
   https://us-central1-feedmapping.cloudfunctions.net/function \
   --data @${ARTIFACT} | jq '.'
-
-curl -L -s -X PATCH \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GH_TOKEN" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/ORGS_JSON \
-  -d "$(jq -n '{name:"ORGS_JSON", value:$value}' --arg value "$(cat "$ARTIFACT")")"
-        
