d19946dd6182682c3c1ed73bf40c8596f3dfe076
eQuantum <8466209+eq19@users.noreply.github.com>
2025-05-16 09:23:13 +0700

    Update app.py

--- user_data/ft_client/test_client/app.py
+++ user_data/ft_client/test_client/app.py
@@ -72,7 +72,7 @@ for num in finished:
     if None in num.colors:
         results.write("%s %s Limit Reached\n" % (num.val, ' '.join([str(c) for c in num.colors])))
     else:
-        results.write("%s %s\n" % (num.val, ' '.join([str(c) for c in num.colors])))
+        results.write("%s %s\n" % (num.val, ', '.join([str(c) for c in num.colors])))
 
 results.close()
 
