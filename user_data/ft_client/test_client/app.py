9789297935e7be930df162545bf67dfc019006e2
eQuantum <8466209+eq19@users.noreply.github.com>
2025-05-18 10:29:28 +0700

    Update app.py

--- user_data/ft_client/test_client/app.py
+++ user_data/ft_client/test_client/app.py
@@ -14,7 +14,7 @@ from pythonCode import *
 from datetime import datetime
 
 
-script, output = argv
+script, output, action_path = argv
 
 # Number parameters for the range of numbers to be tested
 lower_bound = 0#Decimal( str(input("What is the lower bound? ")) )
