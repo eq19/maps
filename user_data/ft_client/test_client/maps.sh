4a24d00099ce6afc5434649b07480a286272e066
eQuantum <8466209+eq19@users.noreply.github.com>
2025-05-18 13:09:08 +0700

    Update maps.sh

--- user_data/ft_client/test_client/maps.sh
+++ user_data/ft_client/test_client/maps.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+# Step 1: Convert TensorFlow SavedModel to MLIR
+iree-import-tf \
+  --savedmodel_dir=add_model \
+  --output_file=add_model.mlir
+
+# Step 2: Compile MLIR to IREE's VMFB (executable binary format)
 iree-compile \
   --iree-input-type=stablehlo \
   --iree-hal-target-backends=llvm-cpu \
-  add_model \
+  add_model.mlir \
   -o add_module.vmfb
+
 #iree-run-module --help
 
 cat $ARTIFACT
