10afcb986bb62c750c7a7317a4375c12e6b8daa6
eQuantum <8466209+eq19@users.noreply.github.com>
2025-05-20 22:50:22 +0700

    Update maps.sh

--- user_data/ft_client/test_client/maps.sh
+++ user_data/ft_client/test_client/maps.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
+#lscpu | grep Flags
 iree-run-module \
   --module=add_module.vmfb \
   --function=serving_default \
   --input="10xf32=[1,2,3,4,5,6,7,8,9,10]" \
   --input="10xf32=[2,3,4,5,6,7,8,9,10,11]"
-
-lscpu | grep Flags
         
 cat $ARTIFACT
 curl -s -X POST \
