a26e8a085a2213a1ecdec2abd9b15b956715af53
eQuantum <8466209+eq19@users.noreply.github.com>
2025-05-20 08:05:18 +0700

    Update app.py

--- user_data/ft_client/test_client/app.py
+++ user_data/ft_client/test_client/app.py
@@ -95,7 +95,7 @@ with open(output_path, "w") as f:
     json.dump(data, f, indent=2)
 
 # Print how long it took
-models.export_model()
+compile.export_model()
 #print(datetime.now() - start_time)
 
 if __name__ == '__main__':
