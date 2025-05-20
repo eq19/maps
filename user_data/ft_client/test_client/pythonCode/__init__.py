12fe320e96623af69ef0171780007e16c58b09bc
eQuantum <8466209+eq19@users.noreply.github.com>
2025-05-20 07:42:44 +0700

    Update __init__.py

--- user_data/ft_client/test_client/pythonCode/__init__.py
+++ user_data/ft_client/test_client/pythonCode/__init__.py
@@ -1,6 +1,6 @@
 __all__ = [
     "jello",      # refers to the 'jello.py' file
-    "models",     # refers to the 'add_models.py' file
+    "models",     # refers to the 'models.py' file
     "utilities",  # refers to the 'utilities.py' file
     #"reverse",   # !!! refers to the 'reverse' function now !!!
 ]
