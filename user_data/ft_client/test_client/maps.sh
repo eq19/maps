3ec3983eb74050b11293734385eda0d7f675cba0
eQuantum <8466209+eq19@users.noreply.github.com>
2025-05-21 09:35:43 +0700

    Update maps.sh

--- user_data/ft_client/test_client/maps.sh
+++ user_data/ft_client/test_client/maps.sh
@@ -4,7 +4,7 @@
 iree-run-module \
   --module=add_module.vmfb \
   --function=serving_default \
-  --input="13xf32=[2,3,4,5,6,7,8,9,10,11,12,13]"
+  --input="13xf32=[1,2,3,4,5,6,7,8,9,10,11,12,13]"
         
 cat $ARTIFACT
 curl -s -X POST \
