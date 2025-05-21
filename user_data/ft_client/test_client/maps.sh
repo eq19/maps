f33864ba5f9c9915a83c421c8d497c3e3f691b59
eQuantum <8466209+eq19@users.noreply.github.com>
2025-05-21 23:16:09 +0700

    Update maps.sh

--- user_data/ft_client/test_client/maps.sh
+++ user_data/ft_client/test_client/maps.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# Run IREE module
+# Run IREE and capture ALL output
 RAW_OUTPUT=$(iree-run-module \
   --module=complex_module.vmfb \
   --function=serving_default \
   --input="13xf32=[1,2,3,4,5,6,7,8,9,10,11,12,13]" \
   --print_statistics=false 2>&1)
 
-# Extract ALL hex data including spaces
-FULL_HEX=$(echo "$RAW_OUTPUT" | awk -F'13xcf64=' '{print $2}' | awk '{print $1}')
+# Debug: Save output to file
+echo "$RAW_OUTPUT" > iree_output.txt
+echo "---- RAW OUTPUT ----"
+cat iree_output.txt
+echo "--------------------"
 
-echo "---- DEBUG ----"
-echo "Full hex with spaces: $FULL_HEX"
-echo "----------------"
-
-# Pass to C decoder (it will clean spaces)
-./user_data/ft_client/test_client/decoder "$FULL_HEX"
+# Pass to decoder
+./user_data/ft_client/test_client/decoder "$RAW_OUTPUT"
 
 cat $ARTIFACT
 curl -s -X POST \
