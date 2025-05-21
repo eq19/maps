7f2e3b7613677643c22483b768e69cc2df691074
eQuantum <8466209+eq19@users.noreply.github.com>
2025-05-21 13:07:24 +0700

    Update maps.sh

--- user_data/ft_client/test_client/maps.sh
+++ user_data/ft_client/test_client/maps.sh
@@ -2,12 +2,10 @@
 
 #lscpu | grep Flags
 iree-run-module \
-  --module=add_module.vmfb \
+  --module=complex_add_module.vmfb \
   --function=serving_default \
   --input="13xf32=[1,2,3,4,5,6,7,8,9,10,11,12,13]" \
-  --output_format=json \
-  --print_statistics=false | \
-  jq -c '.output0[] | [("r"+ (.0|tostring)), ("i"+ (.1|tostring))]'
+  --output=json | jq -c '.output0[] | [("r"+ (.0|tostring)), ("i"+ (.1|tostring))]'
 
 cat $ARTIFACT
 curl -s -X POST \
