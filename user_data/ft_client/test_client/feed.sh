6b95b9aa4f4c36526a6d5341e8926eeef049e211
eQuantum <8466209+eq19@users.noreply.github.com>
2025-06-01 12:10:00 +0700

    Update feed.sh

--- user_data/ft_client/test_client/feed.sh
+++ user_data/ft_client/test_client/feed.sh
@@ -79,19 +79,13 @@ hyperopt() {
     echo "NEW SCORE: $NEW_SCORE"
 
     if (( $(echo "$NEW_SCORE > $OLD_SCORE" | bc -l) )); then
-      cat $STRATEGY
+      cat $PARAMETERS
       curl -L -s -X PATCH \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer $GH_TOKEN" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
          https://api.github.com/repos/$TARGET_REPOSITORY/actions/variables/PARAMS_JSON \
-        -d "$(jq -n '{name:"PARAMS_JSON", value:$value}' --arg value "$(cat "$STRATEGY")")"
-
-      curl -s -X POST \
-        -H "Authorization: Bearer ${BEARER}" \
-        -H "Content-Type: application/json" \
-          https://us-central1-feedmapping.cloudfunctions.net/function \
-        --data @${STRATEGY} | jq '.'
+        -d "$(jq -n '{name:"PARAMS_JSON", value:$value}' --arg value "$(cat "$PARAMETERS")")"
     fi
   done
 }
