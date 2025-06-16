84695fb82a0e1b6c844d835476d89b8181951f8d
eq19 <eq19@users.noreply.github.com>
2025-06-16 08:36:41 +0000

    update workflows

--- .github/entrypoint/artifact.sh
+++ .github/entrypoint/artifact.sh
@@ -55,7 +55,7 @@ set_target() {
   fi
   
   # Generate id from the Structure
-  [[ -z "$SPIN" ]] && if [[ "$1" != "$2" ]]; then SPIN=-1; else SPIN=13; fi
+  [[ -z "$SPIN" ]] && if [[ "$1" != "$2" ]]; then SPIN=13; else SPIN=13; fi
   if [[ -n "$CELL" ]]; then
     SPANPLUS=$(($SPAN + 1))
     if (( $CELL == 0 )); then MOD=7; else MOD=13; fi
