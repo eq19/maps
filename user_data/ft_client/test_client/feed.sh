ccd5ca9d90a910872fb8f631c131b91751cd2f54
eQuantum <8466209+eq19@users.noreply.github.com>
2025-06-17 20:47:15 +0700

    Update feed.sh

--- user_data/ft_client/test_client/feed.sh
+++ user_data/ft_client/test_client/feed.sh
@@ -79,7 +79,7 @@ hyperopt() {
          }')"
     done
 
-    spaces=$(echo "$pipeline" | jq -r '.spaces')  # Combined spaces
+    spaces=$(echo "$pipeline" | jq -r '.spaces | join(" ")')  # Space-separated
     echo -e "\n$hr\nID: $id 👉 Running $loss\nSpaces: $spaces | Days: $days | Epochs: $epochs\n$hr"
     freqtrade hyperopt --timerange ${start_date}-${end_date} --epochs ${epochs} -j 4 \
       --spaces ${spaces} --ignore-missing-spaces --hyperopt-loss ${loss} \
