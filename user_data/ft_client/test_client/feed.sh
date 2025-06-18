952dc70dbc2303864681244f228793adb71aa862
eQuantum <8466209+eq19@users.noreply.github.com>
2025-06-18 21:55:04 +0700

    Update feed.sh

--- user_data/ft_client/test_client/feed.sh
+++ user_data/ft_client/test_client/feed.sh
@@ -46,15 +46,15 @@ hyperopt() {
 
   # Load JSON and filter by given ID
   jq -c --argjson ids "[$(echo "$*" | sed 's/ /,/g')]" '.pipelines[] | select(.id as $id | $ids | index($id))' $HYPERFILE | while read -r pipeline; do
+    end_date=$(date +"%Y%m%d")
+    start_date=$(date -d "$days days ago" +"%Y%m%d")
+    timerange="$start_date-$end_date"
+
     id=$(echo "$pipeline" | jq -r '.id')
     days=$(echo "$pipeline" | jq -r '.days')
     epochs=$(echo "$pipeline" | jq -r '.epochs')
     loss=$(echo "$pipeline" | jq -r '.hyperopt_loss')
     spaces=$(echo "$pipeline" | jq -r '.spaces[]')  # One space per line
-
-    end_date=$(date +"%Y%m%d")
-    start_date=$(date -d "$days days ago" +"%Y%m%d")
-    timerange="$start_date-$end_date"
 
     for space in $spaces; do
       # Extract params as raw JSON
