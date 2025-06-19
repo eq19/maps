383b700f4385b72341c854a17454d41456e636d9
eQuantum <8466209+eq19@users.noreply.github.com>
2025-06-19 16:35:23 +0700

    Update feed.sh

--- user_data/ft_client/test_client/feed.sh
+++ user_data/ft_client/test_client/feed.sh
@@ -47,11 +47,10 @@ hyperopt() {
   # Load JSON and filter by given ID
   jq -c --argjson ids "[$(echo "$*" | sed 's/ /,/g')]" '.pipelines[] | select(.id as $id | $ids | index($id))' $HYPERFILE | while read -r pipeline; do
     end_date=$(date +"%Y%m%d")
+    days=$(echo "$pipeline" | jq -r '.days')
     start_date=$(date -d "$days days ago" +"%Y%m%d")
-    timerange="$start_date-$end_date"
 
     id=$(echo "$pipeline" | jq -r '.id')
-    days=$(echo "$pipeline" | jq -r '.days')
     epochs=$(echo "$pipeline" | jq -r '.epochs')
     loss=$(echo "$pipeline" | jq -r '.hyperopt_loss')
 
