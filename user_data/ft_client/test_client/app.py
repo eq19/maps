53560936e4ee1e3fa53d50777905211227958f67
eQuantum <8466209+eq19@users.noreply.github.com>
2025-06-20 11:25:23 +0700

    Update app.py

--- user_data/ft_client/test_client/app.py
+++ user_data/ft_client/test_client/app.py
@@ -65,18 +65,22 @@ while (current.val <= upper_bound):
 
 
 # write contents to file/process etc.
-output_path = 'user_data/ft_client/test_client/results/{}'.format(output)
+fibbo_path = 'user_data/strategies/fibbo.json'
 params_path = 'user_data/config_examples/config_params.example.json'
+output_path = 'user_data/ft_client/test_client/results/{}'.format(output)
 
-# First load the params JSON file
+# First load the JSON files
+with open(fibbo_path, "r") as fibbo_file:
+    fibbo = json.load(fibbo_file)
+
 with open(params_path, "r") as params_file:
     params = json.load(params_file)
 
-if param != 'nil':
-    params = utilities.set_optimize_flags(params, param)
-
 with open(output_path, "r") as output_file:
     data = json.load(output_file)
+
+if param != 'nil':
+    params = utilities.set_optimize_flags(params, param, fibbo)
 
 counter = 1
 for org in data:
