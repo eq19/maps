f12729a88d6c9047ee00ddb3ac917f5a0306d86a
eQuantum <8466209+eq19@users.noreply.github.com>
2025-06-20 15:00:38 +0700

    Update __init__.py

--- pythonCode/__init__.py
+++ pythonCode/__init__.py
@@ -1,5 +1,6 @@
 __all__ = [
     "jello",      # refers to the 'jello.py' file
+    "params",     # refers to the 'params.py' file
     "compile",    # refers to the 'compile.py' file
     "utilities",  # refers to the 'utilities.py' file
     #"reverse",   # !!! refers to the 'reverse' function now !!!
