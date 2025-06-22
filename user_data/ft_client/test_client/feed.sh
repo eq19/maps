33e67047d1ada2c789075be184d56f9f34f58aa4
eQuantum <8466209+eq19@users.noreply.github.com>
2025-06-22 21:17:32 +0700

    Update feed.sh

--- user_data/ft_client/test_client/feed.sh
+++ user_data/ft_client/test_client/feed.sh
@@ -115,8 +115,8 @@ hyperopt() {
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer $GH_TOKEN" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-         https://api.github.com/repos/$TARGET_REPOSITORY/actions/variables/PARAMS_JSON \
-        -d "$(jq -n '{name:"PARAMS_JSON", value:$value}' --arg value "$(cat "$STRATEGY")")"
+        -d "$(jq -n '{name:"PARAMS_JSON", value:$value}' --arg value "$(cat "$STRATEGY")")" \
+         https://api.github.com/repos/$( [[ "$GITHUB_JOB" == "lexering" ]] && echo "$TARGET_REPOSITORY" || echo "$GITHUB_REPOSITORY" )/actions/variables/PARAMS_JSON
     fi
   done
 }
