1aec984350016f67928faf48452744893dedef80
eQuantum <8466209+eq19@users.noreply.github.com>
2025-07-01 10:08:59 +0700

    Update fibbo.py

--- user_data/strategies/fibbo.py
+++ user_data/strategies/fibbo.py
@@ -418,10 +418,10 @@ class fibbo(IStrategy):
         ATR          = (dataframe['atr'] > dataframe['atr'].shift(1))
         VWAP         = (dataframe['close'] > dataframe['vwap'])
         MACD         = (dataframe["macd"] < dataframe["macdsignal"])
-        DEMA         = (dataframe[f"dema{self.buy_fast_dema.value}"] > dataframe[f"ema{self.buy_slow_ema.value}_{self.informative_timeframe}"])
         FIBBO        = (dataframe['close'].shift(1) < dataframe['fib_618']) & (dataframe['close'] > dataframe['fib_618'])
         BB           = (dataframe["close"] <= dataframe["bb_lowerband"]) #& (dataframe["close"].shift(1) < dataframe["close"])
         STOCHRSI     = (dataframe['fastk_rsi'] > dataframe['fastd_rsi']) & (dataframe['fastk_rsi'] < self.buy_stoch_osc.value)
+        DEMA         = (dataframe[f"dema{self.buy_fast_dema.value}"] > dataframe[f"ema{self.buy_slow_ema.value}_{self.informative_timeframe}"])
 
         long_conditions.append(RSI)
 
