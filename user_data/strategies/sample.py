302b887b48bfd769714bd8254b2912d67e94d47c
eQuantum <8466209+eq19@users.noreply.github.com>
2025-07-09 00:10:10 +0700

    Update and rename AwesomeCombinationStrategy.py to sample.py

--- user_data/strategies/sample.py
+++ user_data/strategies/sample.py
@@ -47,7 +47,7 @@ def indicator_permutations(profiles, max_indicators=1, include_none=True):
     return profile_permutations
 
 
-class AwesomeCombinationStrategy(IStrategy):
+class Sample(IStrategy):
 
     # Strategy interface version - allow new iterations of the strategy interface.
     # Check the documentation or the Sample strategy to get the latest version.
