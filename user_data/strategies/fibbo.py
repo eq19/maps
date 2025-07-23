8e66ba8e5c60a20bd13e63a56aba3af3dbb4ff37
eQuantum <8466209+eq19@users.noreply.github.com>
2025-07-23 10:51:31 +0700

    Update fibbo.py

--- user_data/strategies/fibbo.py
+++ user_data/strategies/fibbo.py
@@ -289,8 +289,9 @@ class Fibbo(IStrategy):
         if hasattr(self, 'max_open_trades') and self.max_open_trades.value != -1:
             self.config['max_open_trades'] = self.max_open_trades.value
 
-        # Check if indodax is the actual exchange to apply the monkey patches
-        if not self.config.get("dry_run", False) and self.dp.exchange.id == 'indodax':
+    def bot_start(self, **kwargs):
+        """Called when strategy is loaded"""
+        if not self.config.get("dry_run", False) and self.exchange.id == 'indodax':
             logger.info("🟢 Live mode with Indodax detected. Applying monkey patches.")
             patch_indodax_create_order()
             patch_indodax_cancel_order()
