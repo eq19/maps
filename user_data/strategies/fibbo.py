6d8518284cb3cd3116c95ff556ec140450cb3dd9
eQuantum <8466209+eq19@users.noreply.github.com>
2025-08-01 10:15:31 +0700

    Update fibbo.py

--- user_data/strategies/fibbo.py
+++ user_data/strategies/fibbo.py
@@ -5,7 +5,6 @@
 import numpy as np  # noqa
 import pandas as pd  # noqa
 from pandas import DataFrame
-from utils.indodax_patch import *
 from typing import Optional, Union
 
 from freqtrade.strategy import (
@@ -34,6 +33,7 @@ import talib.abstract as ta
 import pandas_ta as pd_ta
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 from itertools import permutations
+from utils.indodax_patch import *
 
 
 # Define indicator sets (could also come from the JSON if needed)
