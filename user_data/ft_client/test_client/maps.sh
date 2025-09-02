dccdc0f031cfc936cadf0900beddd6ed19a0284f
eQuantum <8466209+eq19@users.noreply.github.com>
2025-09-02 08:02:02 +0700

    Update maps.sh

--- user_data/ft_client/test_client/maps.sh
+++ user_data/ft_client/test_client/maps.sh
@@ -16,7 +16,7 @@ echo "--------------------"
 # Pass to decoder
 ./float_decoder "$RAW_OUTPUT"
 
-cat $ARTIFACT
+#cat $ARTIFACT
 curl -s -X POST \
   -H "Authorization: Bearer ${BEARER}" \
   -H "Content-Type: application/json" \
