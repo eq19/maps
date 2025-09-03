a1a15502fe4150b857416c2ee92dd0eac70debb8
eQuantum <8466209+eq19@users.noreply.github.com>
2025-09-03 20:41:07 +0700

    Update feed.sh

--- user_data/ft_client/test_client/feed.sh
+++ user_data/ft_client/test_client/feed.sh
@@ -100,8 +100,8 @@ hyperopt() {
     freqtrade hyperopt --timerange ${start_date}-${end_date} --hyperopt-loss ${HYPEROPT:-$loss} -j 4 \
       --spaces ${spaces} --ignore-missing-spaces --epochs ${epochs} --fee=$FEE \
       ${enable_protections} --analyze-per-epoch --random-state ${id} \
-      #--logfile /dev/null > /dev/null 2>&1 
       --print-json
+      #--logfile /dev/null > /dev/null 2>&1 
     freqtrade hyperopt-list
 
     echo -e "\n$hr\nRERUN BACKTEST\n$hr"
