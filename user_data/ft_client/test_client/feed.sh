34713da16b62c6b2f64517f8dcc5d9741c5841b2
eQuantum <8466209+eq19@users.noreply.github.com>
2025-11-16 22:01:39 +0700

    Update workflow trigger with FREQAI_MODEL parameter

--- user_data/ft_client/test_client/feed.sh
+++ user_data/ft_client/test_client/feed.sh
@@ -161,7 +161,7 @@ hyperopt() {
         -d "$(jq -n '{name:"PARAMS_JSON", value:$value}' --arg value "$(cat "$STRATEGY")")" \
          https://api.github.com/repos/$( [[ "$GITHUB_JOB" == "lexering" ]] && echo "$TARGET_REPOSITORY" || echo "$GITHUB_REPOSITORY" )/actions/variables/PARAMS_JSON
       gh variable set HYPEROPT --body "${HYPEROPT:-$loss}" && gh variable set SCORE --body "${NEW_SCORE}" && gh variable set JOB --body "${GITHUB_JOB}"
-      if [[ "$GITHUB_JOB" != "lexering" ]]; then gh workflow run "main.yml"; fi
+      if [[ "$GITHUB_JOB" != "lexering" ]]; then gh workflow run "main.yml" --raw-field "FREQAI_MODEL=$FREQAI_MODEL"; fi
     elif (( $(echo "$NEW_SCORE < $OLD_SCORE" | bc -l) )); then
       if [[ "$GITHUB_JOB" == "lexering" ]] && [[ "$(gh variable get JOB)" != "lexering" ]]; then 
         gh workflow run "main.yml" --raw-field "FREQAI_MODEL=$FREQAI_MODEL"
