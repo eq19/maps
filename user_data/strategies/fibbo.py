a9361c1fd6c1b748111ab6f83f0565f82d179716
eQuantum <8466209+eq19@users.noreply.github.com>
2025-12-14 17:00:53 +0700

    Update fibbo.py

--- user_data/strategies/fibbo.py
+++ user_data/strategies/fibbo.py
@@ -619,7 +619,7 @@ class Fibbo(IStrategy):
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         # Trigger FreqAI pipeline (training/prediction and column injection)
-        df = self.freqai.start(dataframe, metadata, self)
+        #dataframe = self.freqai.start(dataframe, metadata, self)
 
         # RSI 
         dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
