d29020daea010584fa74613e5e5171359a56f721
eQuantum <8466209+eq19@users.noreply.github.com>
2025-12-21 18:31:32 +0700

    Update echo statement to use EPOCHS variable

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -192,7 +192,7 @@ hyperopt() {
         prot="enable"
     fi
 
-    echo -e "\n$hr\nID: $id 👉 Running ${HYPEROPT:-$loss} | Days: $days | Epochs: $epochs\nSpaces: $spaces | FreqAImodel: $FREQAI_MODEL\n$hr"
+    echo -e "\n$hr\nID: $id 👉 Running ${HYPEROPT:-$loss} | Days: $days | Epochs: ${EPOCHS:-$epochs}\nSpaces: $spaces | FreqAImodel: $FREQAI_MODEL\n$hr"
     freqtrade hyperopt --timerange ${start_date}-${end_date} --hyperopt-loss ${HYPEROPT:-$loss} --freqaimodel $FREQAI_MODEL \
       --spaces ${spaces} --ignore-missing-spaces --epochs ${EPOCHS:-$epochs} --fee=$FEE -j 4 \
       --random-state ${id} ${enable_protections} \
