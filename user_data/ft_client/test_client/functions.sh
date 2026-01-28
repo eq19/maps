db86ad62b26b43f1446a1f4653b4c517e221f248
eQuantum <8466209+eq19@users.noreply.github.com>
2026-01-28 21:59:11 +0700

    Enable JSON output for hyperopt command

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -196,9 +196,9 @@ hyperopt() {
     freqtrade hyperopt --timerange ${start_date}-${end_date} --hyperopt-loss ${HYPEROPT:-$loss} --freqaimodel $FREQAI_MODEL \
       --spaces ${spaces} --ignore-missing-spaces --epochs ${epochs} --fee=$FEE -j 4 \
       --random-state ${id} ${enable_protections} \
-      --logfile /dev/null > /dev/null 2>&1
-      #--print-json
-      freqtrade hyperopt-list
+      --print-json
+      #--logfile /dev/null > /dev/null 2>&1
+      #freqtrade hyperopt-list
 
     echo -e "\n$hr\nRERUN BACKTEST with $FREQAI_MODEL\n$hr"
     freqtrade backtesting --help
