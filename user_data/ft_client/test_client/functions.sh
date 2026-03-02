330353f0299ffb30c06630b8ba0739c7a6f9e87d
eQuantum <8466209+eq19@users.noreply.github.com>
2026-03-02 15:46:19 +0700

    Change comparison operator for NEW_SCORE check

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -224,12 +224,12 @@ hyperopt() {
       else
         if [[ "$FREQAI_NEXT" != "false" ]]; then gh workflow run "main.yml" --raw-field "FREQAI_MODEL=$FREQAI_NEXT"; fi      
       fi
-    elif (( $(echo "$NEW_SCORE < $OLD_SCORE" | bc -l) )); then
+    elif (( $(echo "$NEW_SCORE <= $OLD_SCORE" | bc -l) )); then
       if [[ "$GITHUB_JOB" == "lexering" ]]; then
         if [[ "$(gh variable get JOB)" != "lexering" ]]; then
           gh workflow run "main.yml" --raw-field "FREQAI_MODEL=$FREQAI_MODEL" --raw-field "REDUCE_EPOCH=$REDUCE_EPOCH"
         else
-          if [[ "$FREQAI_NEXT" != "false" ]]; then gh workflow run "main.yml" --raw-field "FREQAI_MODEL=$FREQAI_NEXT"; fi
+          if [[ "$CURRENT" == "false" ]] || [[ "$FREQAI_NEXT" != "false" ]]; then gh workflow run "main.yml" --raw-field "FREQAI_MODEL=$FREQAI_NEXT"; fi
         fi
       fi
     fi
