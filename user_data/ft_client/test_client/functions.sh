13834374fee5747d1e80fc417a4b0ec56cbf0aaa
eQuantum <8466209+eq19@users.noreply.github.com>
2026-03-02 15:53:06 +0700

    Fix logical condition for running workflow

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -229,7 +229,7 @@ hyperopt() {
         if [[ "$(gh variable get JOB)" != "lexering" ]]; then
           gh workflow run "main.yml" --raw-field "FREQAI_MODEL=$FREQAI_MODEL" --raw-field "REDUCE_EPOCH=$REDUCE_EPOCH"
         else
-          if [[ "$CURRENT" == "false" ]] || [[ "$FREQAI_NEXT" != "false" ]]; then gh workflow run "main.yml" --raw-field "FREQAI_MODEL=$FREQAI_NEXT"; fi
+          if [[ "$CURRENT" == "false" || "$FREQAI_NEXT" != "false" ]]; then gh workflow run "main.yml" --raw-field "FREQAI_MODEL=$FREQAI_NEXT"; fi
         fi
       fi
     fi
