10b2e3c62d88de1fd24aa7f0066a1b75daa283aa
eQuantum <8466209+eq19@users.noreply.github.com>
2026-03-04 10:41:38 +0700

    Add cat command to display strategy file

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -200,6 +200,7 @@ hyperopt() {
       --logfile /dev/null > /dev/null 2>&1
       #--print-json
     #freqtrade hyperopt-list
+    cat "$STRATEGY"
 
     echo -e "\n$hr\nRERUN BACKTEST with $FREQAI_MODEL\n$hr"
     freqtrade backtesting --help
