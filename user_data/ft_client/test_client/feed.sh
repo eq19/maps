7f78085f2752adb0f814a099019a4c03b0ba4ba4
eQuantum <8466209+eq19@users.noreply.github.com>
2026-03-18 09:09:21 +0700

    Update feed.sh

--- user_data/ft_client/test_client/feed.sh
+++ user_data/ft_client/test_client/feed.sh
@@ -222,9 +222,5 @@ else
   echo -e "\n$hr\nRUN HYPEROPT with $FREQAI_MODEL\n$hr"
   #Ref: https://www.freqtrade.io/en/stable/hyperopt
   freqtrade hyperopt --help
-  if [[ "$REDUCE_EPOCH" == "false" ]]; then
-    hyperopt $ID
-  else
-    if [[ "$FREQAI_NEXT" != "false" ]]; then gh workflow run "main.yml" --raw-field "FREQAI_MODEL=$FREQAI_NEXT"; fi
-  fi
+  hyperopt $ID
 fi
