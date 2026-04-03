a1ca2c1ecfd25d812f9e876b7100bc3f0e243ba6
eQuantum <8466209+eq19@users.noreply.github.com>
2026-04-03 21:20:33 +0700

    Refactor profit_mean_pct to profit_mean in scoring

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -56,18 +56,17 @@ calculate_score() {
     return
   fi
 
-  [[ $(echo "$profit_mean_pct > 2.5" | bc -l) -eq 1 ]] && profit_mean_pct=2.5
   [[ $(echo "$cagr > 1.0" | bc -l) -eq 1 ]] && cagr=1.0
   [[ $(echo "$expectancy > 1.0" | bc -l) -eq 1 ]] && expectancy=1.0
   [[ $(echo "$profit_total_pct > 25" | bc -l) -eq 1 ]] && profit_total_pct=25
 
-  [[ $(echo "$profit_mean_pct < 0" | bc -l) -eq 1 ]] && profit_mean_pct=0
+  [[ $(echo "$profit_mean < 0" | bc -l) -eq 1 ]] && profit_mean=0
   [[ $(echo "$profit_total_pct < 0" | bc -l) -eq 1 ]] && profit_total_pct=0
   [[ $(echo "$cagr < 0" | bc -l) -eq 1 ]] && cagr=0
   [[ $(echo "$expectancy < 0" | bc -l) -eq 1 ]] && expectancy=0
 
   local winrate_score=$(echo "$winrate * 25" | bc -l)
-  local profit_mean_score=$(echo "$profit_mean_pct * 10" | bc -l)
+  local profit_mean_score=$(echo "$profit_mean * $trades * 25" | bc -l)
   local profit_total_score=$(echo "$profit_total_pct * 1" | bc -l)
   local cagr_score=$(echo "$cagr * 10" | bc -l)
   local expectancy_score=$(echo "$expectancy * 5" | bc -l)
