c995584338aafcfec45445f6c57fd061c801e14c
eQuantum <8466209+eq19@users.noreply.github.com>
2026-04-05 10:25:21 +0700

    Update feed.sh

--- user_data/ft_client/test_client/feed.sh
+++ user_data/ft_client/test_client/feed.sh
@@ -207,7 +207,7 @@ else
           gh variable set SCORE --body "${SCORE}"
         elif (( $(echo "$SCORE > $OLD_SCORE" | bc -l) )); then
           gh variable set SCORE --body "${SCORE}"
-          gh variable set FREQAI_MODEL --body "${$FREQAI_MODEL}"                 
+          gh variable set FREQAIMODEL --body "${$FREQAI_MODEL}"                 
         fi
         export CALCULATION="false"
       fi
