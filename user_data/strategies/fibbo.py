e8f55aa90a71ff637094f4cca945a347e2acb08d
eQuantum <8466209+eq19@users.noreply.github.com>
2026-04-10 00:53:53 +0700

    Update fibbo.py

--- user_data/strategies/fibbo.py
+++ user_data/strategies/fibbo.py
@@ -36,9 +36,7 @@ import pandas_ta as pd_ta
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 from itertools import permutations
 from utils import (
-    BLACKLISTED_PAIRS,
-    _spread_blocked_pairs,
-    patch_ccxt_create_order,
+    patch_ccxt_all(),
     patch_indodax_create_order,
     patch_indodax_cancel_order,
     patch_indodax_fetch_order,
