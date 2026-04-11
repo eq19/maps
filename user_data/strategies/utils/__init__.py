d591749e1dccf3dd88415d9d3d43727bafb0f98c
eQuantum <8466209+eq19@users.noreply.github.com>
2026-04-11 08:20:21 +0700

    Update __init__.py

--- user_data/strategies/utils/__init__.py
+++ user_data/strategies/utils/__init__.py
@@ -1,5 +1,5 @@
 from .ccxt_patch import (
-    patch_ccxt_all,
+    patch_ccxt_pair_only,
 )
 
 from .indodax_patch import (
@@ -8,7 +8,7 @@ from .indodax_patch import (
 )
 
 __all__ = [
-    "patch_ccxt_all",
+    "patch_ccxt_pair_only",
     "patch_indodax_cancel_order",
     "patch_indodax_fetch_order",
 ]
