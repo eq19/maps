75c1f145b234c2c21a7ef87808896a05c368c2e8
eQuantum <8466209+eq19@users.noreply.github.com>
2026-04-13 12:22:33 +0700

    Create sklearn_adapter.py

---
+++ user_data/freqaimodels/adapters/sklearn_adapter.py
@@ -0,0 +1,31 @@
+from pandas import DataFrame
+import numpy as np
+
+from .base_adapter import BaseAdapter
+
+
+class SklearnAdapter(BaseAdapter):
+
+    def fit(self, data_dictionary, dk, **kwargs):
+        X = data_dictionary["train_features"]
+        y = data_dictionary["train_labels"]
+
+        self.model.fit(X, y)
+        return self.model
+
+    def predict(self, unfiltered_df, dk, **kwargs):
+        dk.find_features(unfiltered_df)
+
+        X = dk.data_dictionary["prediction_features"]
+
+        preds = self.model.predict(X)
+
+        # Ensure 2D
+        if len(preds.shape) == 1:
+            preds = preds.reshape(-1, 1)
+
+        pred_df = DataFrame(preds, columns=dk.label_list)
+
+        do_predict = np.ones(len(pred_df), dtype=int)
+
+        return pred_df, do_predict
