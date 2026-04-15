758dfcd43facf9d45ce0801e36c2e7ca37dd42cb
eQuantum <8466209+eq19@users.noreply.github.com>
2026-04-15 13:05:34 +0700

    Update functions.sh

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -382,8 +382,8 @@ hyperopt() {
 
     echo -e "\n$hr\nID: $id | FreqAImodel: $FREQAI_MODEL | Days: $days | Epochs: $epochs\n👉 Running ${HYPEROPT:-$loss} | Spaces: $spaces\n$hr"
     if freqtrade hyperopt --timerange ${start_date}-${end_date} --hyperopt-loss ${HYPEROPT:-$loss} --freqaimodel $FREQAI_MODEL \
-      --spaces ${spaces} --ignore-missing-spaces --epochs ${epochs} --fee=$FEE -j 4 \
-      --random-state ${id} ${enable_protections} --logfile /dev/null; then
+      --spaces ${spaces} --ignore-missing-spaces --epochs ${epochs} --fee=$FEE -j 4 --logfile /dev/null\
+      --random-state ${id} ${enable_protections} > /dev/null 2>&1; then
       freqtrade hyperopt-list --best
     else
       echo "Hyperopt failed, continuing anyway..."
