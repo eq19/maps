0e86823ee4ea59a9091e2866f99b7f6fdc6e17e6
eQuantum <8466209+eq19@users.noreply.github.com>
2026-04-24 19:49:11 +0700

    Update timerange parameter for download-data command

--- user_data/ft_client/test_client/feed.sh
+++ user_data/ft_client/test_client/feed.sh
@@ -108,7 +108,7 @@ if [[ "$1" != "hyperopt" ]]; then
     echo -e "\n$hr\nDOWNLOAD PAIRS\n$hr"
     freqtrade download-data --help
     freqtrade test-pairlist --one-column 2>/dev/null | tail -n +2 | jq -R . | jq -s . > pairs.json
-    freqtrade download-data --pairs-file pairs.json --timeframes $TIMEFRAMES --timerange="$(date -u -d "3 months ago" +%Y%m%d)-$(date -u +%Y%m%d)" --verbose
+    freqtrade download-data --pairs-file pairs.json --timeframes $TIMEFRAMES --timerange="$TD" --verbose
   fi
 
 else
