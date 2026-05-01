507d79430a8cfc8042f9ff241ecc7b92f564e747
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-02 05:05:07 +0700

    Modify jq commands in feed.sh
    Comment out a jq command for updating the exchange pair whitelist and adjust the jq command for frequency AI file.

--- user_data/ft_client/test_client/feed.sh
+++ user_data/ft_client/test_client/feed.sh
@@ -118,8 +118,8 @@ else
   export CALCULATION="false"
   pairs=$(gh variable get PAIRS)
   #jq '.pairlists = [{"method": "StaticPairList"}]' $PAIRFILE > tmp.json && mv tmp.json $PAIRFILE  
-  jq --argjson pairs "$pairs" '.include_corr_pairlist = $pairs' "$FREQAI_FILE" > freqai.tmp && mv freqai.tmp "$FREQAI_FILE"
-  jq --argjson pairs "$pairs" '.exchange.pair_whitelist = $pairs' "$EXCHANGE_FILE" > config.tmp && mv config.tmp "$EXCHANGE_FILE"
+  #jq --argjson pairs "$pairs" '.exchange.pair_whitelist = $pairs' "$EXCHANGE_FILE" > config.tmp && mv config.tmp "$EXCHANGE_FILE"
+  jq --argjson pairs "$pairs" 'freqai.feature_parameters.include_corr_pairlist = $pairs' "$FREQAI_FILE" > freqai.tmp && mv freqai.tmp "$FREQAI_FILE"
 
   if [[ "$GITHUB_JOB" == "lexering" ]]; then
 
