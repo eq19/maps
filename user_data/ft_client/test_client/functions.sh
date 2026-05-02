ec28f696dbd6206de047fab42bce68b3b76b033d
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-02 07:00:31 +0700

    Refactor backtesting command and improve echo output
    Updated backtesting command to include dynamic pairlist and fixed echo command.

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -395,9 +395,8 @@ hyperopt() {
       freqtrade hyperopt-list --best
     fi
 
-    echo -e "\n$hr\nRERUN BACKTEST ($TB) with $FREQAI_MODEL\n$hr"
-    freqtrade backtesting --help
-    #rm -rf user_data/backtest_results/*
+    echo -e "\n$hr\nRERUN BACKTEST ($TB) with $FREQAI_MODEL\n$hr" && freqtrade backtesting --help
+    jq '.pairlists = [{"method": "StaticPairList"}]' $PAIRFILE > tmp.json && mv tmp.json $PAIRFILE
     freqtrade backtesting --freqaimodel $FREQAI_MODEL --fee=$FEE --timerange="$TB" --enable-dynamic-pairlist --enable-protections
   
     calculate_score
