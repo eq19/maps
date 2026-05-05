9efb6267986a2059522d8a2faa66e173fa01e399
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-05 16:31:18 +0700

    Disable freqai feature and update pairlists
    Disable frequency AI feature and update configuration files.

--- user_data/ft_client/test_client/feed.sh
+++ user_data/ft_client/test_client/feed.sh
@@ -80,9 +80,9 @@ elif [[ "$1" == "Hyperopt" ]]; then
   OLD_SCORE=$SCORE
   export CALCULATION="false"
   pairs=$(gh variable get PAIRS)
+  jq '.freqai.enable = false' "$FREQAI_FILE" > freqai.tmp && mv freqai.tmp "$FREQAI_FILE"
   jq '.pairlists = [{"method": "StaticPairList"}]' $PAIRFILE > tmp.json && mv tmp.json $PAIRFILE  
   jq --argjson pairs "$pairs" '.exchange.pair_whitelist = $pairs' "$EXCHANGE_FILE" > config.tmp && mv config.tmp "$EXCHANGE_FILE"
-  jq --argjson pairs "$pairs" '.freqai.feature_parameters.include_corr_pairlist = $pairs' "$FREQAI_FILE" > freqai.tmp && mv freqai.tmp "$FREQAI_FILE"
 
  if [[ "$GITHUB_JOB" == "lexering" ]]; then
 
