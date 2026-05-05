eeade310d2381a2d6adca297fd9bd9fb59d7e05e
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-05 16:48:02 +0700

    Add HYPEROPT and FREQAI_MODEL variable initialization

--- user_data/ft_client/test_client/feed.sh
+++ user_data/ft_client/test_client/feed.sh
@@ -30,6 +30,7 @@ TD="$EARLIEST_DATE-$TODAY"
 TB="$BACKTESTING_START-$TODAY"
 
 # Print the timeranges
+HYPEROPT=${HYPEROPT:-$(gh variable get HYPEROPT)}
 FREQAI_MODEL=${FREQAI_MODEL:-$(gh variable get FREQAIMODEL)}
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/functions.sh"
