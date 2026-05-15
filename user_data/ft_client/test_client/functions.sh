88a4fbcb56679951d913945013eaad4fb785624a
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-15 10:41:39 +0700

    Fix conditional logic for GITHUB_JOB handling

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -546,10 +546,10 @@ freqai() {
           -d "$(jq -n '{name:"PARAMS_JSON", value:$value}' --arg value "$(cat "$STRATEGY")")" \
            https://api.github.com/repos/$( [[ "$GITHUB_JOB" == "lexering" ]] && echo "$TARGET_REPOSITORY" || echo "$GITHUB_REPOSITORY" )/actions/variables/PARAMS_JSON
         gh variable set FREQAIMODEL --body "${FREQAI_MODEL}" && gh variable set SCORE --body "${NEW_SCORE}"
-        if [[ "$GITHUB_JOB" == "lexering" ]]; then
+        if [[ "$GITHUB_JOB" != "lexering"; then
+          gh variable set JOB --body "${GITHUB_JOB}"
+        elif [[ "$GITHUB_JOB" == "lexering" ]]; then
           gh workflow run "main.yml" --raw-field "RUN_MODE=MEC30" --raw-field "BYPASS_LEXERING=true"   
-        elif [[ "$GITHUB_JOB" != "lexering" &&  "$(gh variable get JOB)" == "lexering" ]]; then
-          gh variable set JOB --body "${GITHUB_JOB}" && gh workflow run "main.yml" --raw-field "RUN_MODE=FreqAI" --raw-field "REDUCE_EPOCH=$REDUCE_EPOCH"
         fi
       elif (( $(echo "$NEW_SCORE < $OLD_SCORE" | bc -l) )); then
         if [[ "$GITHUB_JOB" == "lexering" ]]; then
