a9b95c78277d09cefe3f9e900ef1a74f1849d7ad
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-15 12:31:08 +0700

    Update error handling in backtesting function

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -526,7 +526,7 @@ freqai() {
     freqtrade backtesting --freqaimodel $FREQAI_MODEL --fee=$FEE --timerange="$TB" --enable-protections --log-file backtest.log
   
     # Execute calculate_score ONLY if no errors and exit code 0
-    if [ $? -eq 0 ] && ! grep -qE "(ERROR|WARNING)" backtest.log; then
+    if [ $? -eq 0 ] && ! grep -qiE "(error|traceback)" backtest.log; then
 
       export CALCULATION="false"
       OLD_SCORE=$SCORE            
