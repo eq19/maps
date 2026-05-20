cacab0d5df35aab041a123709866799b9a84c817
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-20 18:39:55 +0700

    Enhance FreqAI model listing command
    Updated freqtrade command to include multiple model paths for FreqAI mode.

--- user_data/ft_client/test_client/feed.sh
+++ user_data/ft_client/test_client/feed.sh
@@ -70,8 +70,12 @@ if [[ "$1" != "Hyperopt" &&  "$1" != "FreqAI" ]]; then
       freqtrade list-hyperoptloss
     elif [[ "$RUN_MODE" == "FreqAI" ]]; then
       freqtrade list-freqaimodels --help
-      freqtrade list-freqaimodels
-    fi
+      freqtrade list-freqaimodels \
+        --freqaimodel-path user_data/freqaimodels/resources \
+        --freqaimodel-path user_data/freqaimodels/tradeflow \
+        --freqaimodel-path user_data/freqaimodels/torch_models \
+        --freqaimodel-path user_data/freqaimodels/tensorflow_models
+       fi
 
     echo -e "\n$hr\nDOWNLOAD PAIRS ($TD)\n$hr"
     freqtrade download-data --help
