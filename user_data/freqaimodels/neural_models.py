00799201609bdf82d626d5271f59a71263f402a5
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-20 21:12:54 +0700

    Rename PyTorchTransformerRegressor to PyTorchNMTransformerRegressor

--- user_data/freqaimodels/neural_models.py
+++ user_data/freqaimodels/neural_models.py
@@ -214,7 +214,7 @@ class TransformerModel(nn.Module):
         return self.fc(x).squeeze(-1)
 
 
-class PyTorchTransformerRegressor(BaseRegressionModel):
+class PyTorchNMTransformerRegressor(BaseRegressionModel):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
