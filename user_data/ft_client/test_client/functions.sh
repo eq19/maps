6d36f6f58644a402ad17613a2b544429644420b4
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-21 12:22:51 +0700

    Update freqaimodels path in functions.sh

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -462,7 +462,7 @@ hyperopt() {
 freqai() {
 
   # Extract clean list of hyperoptloss classes
-  freqaimodels=$(printf '%s\n' "$(freqtrade list-freqaimodels --one-column)" | jq -R . | jq -s .)
+  freqaimodels=$(printf '%s\n' "$(freqtrade list-freqaimodels --freqaimodel-path $FREQAIMODELS_PATH --one-column)" | jq -R . | jq -s .)
   
   # Load JSON and filter by given ID
   jq -c --argjson ids "[$(echo "$*" | sed 's/ /,/g')]" '.pipelines[] | select(.id as $id | $ids | index($id))' $HYPERFILE | while read -r pipeline; do
@@ -518,7 +518,7 @@ freqai() {
     #jq --argjson pairs "$pairs" '.freqai.feature_parameters.include_corr_pairlist = $pairs' "$FREQAI_FILE" > freqai.tmp && mv freqai.tmp "$FREQAI_FILE"
 
     echo -e "\n$hr\nAI TRADES with $FREQAI_MODEL\n$hr" && freqtrade trade --help
-    nohup freqtrade trade -v --dry-run --freqaimodel $FREQAI_MODEL --freqaimodel-path "$FREQAIMODELS_PATH" --fee=$FEE > freqtrade.log 2>&1 &
+    nohup freqtrade trade -v --dry-run --freqaimodel $FREQAI_MODEL --freqaimodel-path $FREQAIMODELS_PATH --fee=$FEE > freqtrade.log 2>&1 &
     echo $! > freqtrade_pid.txt
 
     # Open descriptor to log stream
@@ -541,7 +541,7 @@ freqai() {
     SCORE=$(gh variable get SCORE)
     freqtrade backtesting --help
     jq '.pairlists = [{"method": "StaticPairList"}]' $PAIRFILE > tmp.json && mv tmp.json $PAIRFILE
-    freqtrade backtesting --freqaimodel $FREQAI_MODEL --freqaimodel-path "$FREQAIMODELS_PATH" --fee=$FEE --timerange="$TB" --enable-protections --log-file backtest.log
+    freqtrade backtesting --freqaimodel $FREQAI_MODEL --freqaimodel-path $FREQAIMODELS_PATH --fee=$FEE --timerange="$TB" --enable-protections --log-file backtest.log
   
     # Execute calculate_score ONLY if no errors and exit code 0
     if [ $? -eq 0 ] && ! grep -qiE "(Error|Traceback|No further splits with positive gain)" backtest.log; then
