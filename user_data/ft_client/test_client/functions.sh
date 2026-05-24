faa2ae696b81c8ed4ee852900f8373b963bc9957
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-25 02:01:29 +0700

    Update functions.sh

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -501,7 +501,7 @@ freqai() {
         "https://api.github.com/repos/$GITHUB_REPOSITORY/actions/workflows/matrix.yml/dispatches"
       gh variable set JOB --body "${GITHUB_JOB}"
  
-      jq '.pairlists |= map(if .method == "VolumePairList" then .number_assets = 100 else . end)' $PAIRFILE > tmp.json && mv tmp.json $PAIRFILE
+      jq '.pairlists |= map(if .method == "VolumePairList" then .number_assets = 150 else . end)' $PAIRFILE > tmp.json && mv tmp.json $PAIRFILE
       freqtrade test-pairlist --one-column 2>/dev/null | tail -n +2 | jq -R . | jq -s . > pairs.json
       freqtrade download-data --pairs-file pairs.json --timeframes $TIMEFRAMES --timerange="$TD" --verbose
 
