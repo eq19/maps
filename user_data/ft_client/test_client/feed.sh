c9b8804e5598c83136a8d5a279f3f4ba31920f82
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-27 12:48:58 +0700

    Update feed.sh

--- user_data/ft_client/test_client/feed.sh
+++ user_data/ft_client/test_client/feed.sh
@@ -42,7 +42,7 @@ export PATH="venv/bin:$PATH"
 export PYTHONPATH="user_data/strategies:user_data/freqaimodels:$PYTHONPATH"
 
 if [[ ( $# -eq 0  && "$GITHUB_JOB" == "lexering" ) || ( "$1" == "FreqAI" && "$GITHUB_JOB" != "lexering" ) ]]; then
-  pip install -qq --no-cache-dir optuna optunahub neptune scikit-image wandb tscv numba mlflow feature_engine
+  pip install -qq --no-cache-dir optuna optunahub neptune scikit-image wandb tscv numba mlflow feature_engine shap
 fi
 
 if [[ $# -eq 0 ]]; then
