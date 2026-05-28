f7488cb98812977a739e8e9865c384afebbd5342
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-28 09:05:12 +0700

    Update requirements-freqaimodels.py

--- user_data/freqaimodels/standalone/utils/requirements-freqaimodels.py
+++ user_data/freqaimodels/standalone/utils/requirements-freqaimodels.py
@@ -1,13 +1,13 @@
-feature_engine
-neptune
-numba
-mlflow
-optuna
-optunahub
-pangres
+feature_engine==1.9.4
+neptune==1.14.0
+numba==0.65.1
+mlflow==3.1.0
+optuna==4.4.0
+optunahub==0.3.1
+pangres==4.2.1
 # probatus
-scikit-image
+scikit-image==0.25.2
 # scikit-learn-extra
-shap
+shap==0.49.1
 # tscv
-wandb
+wandb==0.21.1
