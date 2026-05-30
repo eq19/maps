96cf2aafa997cd33439f1402e82cf2c464d4562d
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-30 07:38:36 +0700

    Update maps.sh

--- user_data/ft_client/test_client/maps.sh
+++ user_data/ft_client/test_client/maps.sh
@@ -31,7 +31,7 @@ echo "--------------------"
 
 # Pass to decoder
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-[[ "$JOBS_ID" == "1" ]] && $SCRIPT_DIR/float_decoder "$RAW_OUTPUT"
+[[ "$JOBS_ID" == "1" ]] && ./float_decoder "$RAW_OUTPUT"
 
 while true; do
   #echo "Attempt $((COUNT+1))..."
