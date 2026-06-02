861cf468977915085e31e5d6c6fd190226016c2f
eQuantum <8466209+eq19@users.noreply.github.com>
2026-06-02 20:48:23 +0700

    Remove redundant cat command in functions.sh
    Remove unnecessary cat command for STRATEGY file.

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -384,7 +384,7 @@ hyperopt() {
         enable_protections="--enable-protections"
         prot="enable"
     fi
-cat $STRATEGY
+
     echo -e "\n$hr\nID: $id 👉 Running ${HYPEROPT:-$loss}\nSpaces: $spaces | Days: $days | Epochs: $epochs\n$hr"
     if ! error=$(freqtrade hyperopt --timerange ${start_date}-${end_date} --hyperopt-loss ${HYPEROPT:-$loss} \
       --spaces ${spaces} --ignore-missing-spaces --epochs ${epochs} --fee=$FEE -j 4 --logfile /dev/null \
@@ -397,7 +397,6 @@ cat $STRATEGY
     
     echo -e "\n$hr\nRERUN BACKTEST ($TB) without FREQAI_MODEL\n$hr" && freqtrade backtesting --help
     jq '.params.roi |= with_entries(select(.key | startswith("roi_t") | not))' "$STRATEGY" > tmp.$$ && mv tmp.$$ "$STRATEGY"
-cat $STRATEGY
     freqtrade backtesting --fee=$FEE --timerange="$TB" --enable-protection
 
     calculate_score
