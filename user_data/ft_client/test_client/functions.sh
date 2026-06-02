559de8f87aec7da2eef32dc9e0757807f82b6f71
eQuantum <8466209+eq19@users.noreply.github.com>
2026-06-02 20:28:00 +0700

    Refactor strategy file handling in functions.sh
    Remove unnecessary cat command and add jq processing for strategy file.

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -394,8 +394,10 @@ cat $STRATEGY
     else
       freqtrade hyperopt-list --best
     fi
-cat $STRATEGY    
+    
     echo -e "\n$hr\nRERUN BACKTEST ($TB) without FREQAI_MODEL\n$hr" && freqtrade backtesting --help
+    jq '.params.roi |= with_entries(select(.key | startswith("roi_t") | not))' "$STRATEGY" > tmp.$$ && mv tmp.$$ "$STRATEGY"
+cat $STRATEGY
     freqtrade backtesting --fee=$FEE --timerange="$TB" --enable-protection
 
     calculate_score
