78b186b5f19eecd9b1215ec10669a2f468ab2684
eQuantum <8466209+eq19@users.noreply.github.com>
2026-06-05 14:15:16 +0700

    Refactor winrate calculations and scores

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -81,10 +81,11 @@ calculate_score() {
   echo "📈 Strategy Summary for 'Fibbo'"
   echo "---------------------------------"
 
-  local profit_total_score=$(echo "$profit_total_pct * 1" | bc -l)
-  [[ $(echo "$profit_total_pct < 0" | bc -l) -eq 1 ]] && profit_total_score=0
-  [[ $(echo "$profit_total_pct > 20" | bc -l) -eq 1 ]] && profit_total_score=20
-  echo "💰 1.1 Profit Total: $profit_total_pct% (score: $(printf "%.2f" "$profit_total_score") of 20)"
+  WINRATE=$(echo "$winrate * 100" | bc -l)
+  WINRATE=$(printf "%.2f" "$WINRATE")
+
+  local winrate_score=$(echo "$winrate * 20" | bc -l)
+  echo "📊 1.1 Winrate: $WINRATE% (score: $(printf "%.2f" "$winrate_score") of 20)"
 
   [[ $(echo "$profit_mean < 0" | bc -l) -eq 1 ]] && profit_mean=0
   local profit_mean_score=$(echo "
@@ -118,13 +119,12 @@ calculate_score() {
 
   echo "💰 1.2 Profit Mean: $profit_mean_pct% (score: $(printf "%.2f" "$profit_mean_score") of 10)"
 
-  WINRATE=$(echo "$winrate * 100" | bc -l)
-  WINRATE=$(printf "%.2f" "$WINRATE")
+  local profit_total_score=$(echo "$profit_total_pct * 1" | bc -l)
+  [[ $(echo "$profit_total_pct < 0" | bc -l) -eq 1 ]] && profit_total_score=0
+  [[ $(echo "$profit_total_pct > 20" | bc -l) -eq 1 ]] && profit_total_score=10
+  echo "💰 1.3 Profit Total: $profit_total_pct% (score: $(printf "%.2f" "$profit_total_score") of 10)"
 
-  local winrate_score=$(echo "$winrate * 10" | bc -l)
-  echo "📊 1.3 Winrate: $WINRATE% (score: $(printf "%.2f" "$winrate_score") of 10)"
-
-  local profit=$(echo "$profit_total_score + $profit_mean_score + $winrate_score" | bc -l)
+  local profit=$(echo "$winrate_score + $profit_mean_score + $profit_total_score" | bc -l)
   echo "📊 Profit Block: $(printf "%.2f" "$profit") of 40"
   echo ""
 
