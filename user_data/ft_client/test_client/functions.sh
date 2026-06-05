10df0c2ea6fecc317165c49e18af474c6e0cb67d
eQuantum <8466209+eq19@users.noreply.github.com>
2026-06-05 14:30:02 +0700

    Adjust profit_total_score threshold from 20 to 10

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -121,7 +121,7 @@ calculate_score() {
 
   local profit_total_score=$(echo "$profit_total_pct * 1" | bc -l)
   [[ $(echo "$profit_total_pct < 0" | bc -l) -eq 1 ]] && profit_total_score=0
-  [[ $(echo "$profit_total_pct > 20" | bc -l) -eq 1 ]] && profit_total_score=10
+  [[ $(echo "$profit_total_pct > 10" | bc -l) -eq 1 ]] && profit_total_score=10
   echo "💰 1.3 Profit Total: $profit_total_pct% (score: $(printf "%.2f" "$profit_total_score") of 10)"
 
   local profit=$(echo "$winrate_score + $profit_mean_score + $profit_total_score" | bc -l)
