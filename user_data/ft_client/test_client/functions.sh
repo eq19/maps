96a0ca3ca17d8e167541700e37d3380bac456252
eQuantum <8466209+eq19@users.noreply.github.com>
2026-06-17 04:04:15 +0700

    Update functions.sh

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -376,7 +376,7 @@ hyperopt() {
           }}')" \
         "https://api.github.com/repos/$GITHUB_REPOSITORY/actions/workflows/matrix.yml/dispatches"
       gh variable set JOB --body "${GITHUB_JOB}"
-      hyperopt_loss="SharpeSortinoHyperOptLoss"
+      hyperopt_loss="OnlyProfitHyperOptLoss"
       epochs=$((epochs * 2))
     else
       hyperopt_loss="$HYPEROPT"
