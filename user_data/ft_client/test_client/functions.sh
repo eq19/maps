ae597cbc593748e6e79b6eb37aba6f37c5f89b8a
eQuantum <8466209+eq19@users.noreply.github.com>
2026-06-19 13:25:31 +0700

    Update functions.sh

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -99,9 +99,9 @@ calculate_score() {
 
   echo "💰 1.2 Profit Mean: $profit_mean_pct% (score: $(printf "%.2f" "$profit_mean_score") of 10)"
 
-  local profit_total_score=$(echo "$profit_total_pct * 0.4" | bc -l)
+  local profit_total_score=$(echo "$profit_total_pct * 0.2" | bc -l)
   [[ $(echo "$profit_total_pct < 0" | bc -l) -eq 1 ]] && profit_total_score=0
-  [[ $(echo "$profit_total_pct > 25" | bc -l) -eq 1 ]] && profit_total_score=10
+  [[ $(echo "$profit_total_pct > 50" | bc -l) -eq 1 ]] && profit_total_score=10
   echo "💰 1.3 Profit Total: $profit_total_pct% (score: $(printf "%.2f" "$profit_total_score") of 10)"
 
   local profit_factor_score=$(echo "
