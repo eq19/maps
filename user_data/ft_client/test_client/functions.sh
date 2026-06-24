7932f282f604b1f81312b5e46e2c15d6a01bc42f
eQuantum <8466209+eq19@users.noreply.github.com>
2026-06-24 17:18:29 +0700

    Update functions.sh

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -319,7 +319,7 @@ calculate_score() {
 
   if (( $(echo "$expectancy_ratio >= 0" | bc -l) && $(echo "$sortino >= 0" | bc -l) )); then
     SCORE=$(echo "($expectancy_ratio * $sortino) / $max_drawdown_account" | bc -l)
-    SCORE=$(printf "%.2f" "$SCORE")
+    SCORE=$(printf "%.2f" $( echo "$SCORE / 10" | bc -l))
     echo "✅ SCORE: $SCORE"
   fi
 
