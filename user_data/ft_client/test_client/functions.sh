abce20394db7f76c3e15d1eea1a3bb9b23d8fd84
eQuantum <8466209+eq19@users.noreply.github.com>
2026-07-03 18:29:26 +0700

    Modify TRADES_MIN value assignment logic
    Adjust TRADES_MIN based on RUN_MODE condition.

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -6,7 +6,7 @@
 calculate_score() {
 
   # Scoring breakdown:
-  local TRADES_MIN=300
+  [[ "$RUN_MODE" == "FreqAI" ]] && local TRADES_MIN=300 || local TRADES_MIN=400
 
   sleep 5
   local dir="user_data/backtest_results"
