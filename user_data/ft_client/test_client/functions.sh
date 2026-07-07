3ddc41fb526d10ea1c22f58b7ac1671520f64634
eQuantum <8466209+eq19@users.noreply.github.com>
2026-07-07 09:33:12 +0700

    Replace break and return with exit in functions.sh

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -589,8 +589,7 @@ freqai() {
         PID=$(cat freqtrade_pid.txt)
         kill -SIGTERM $PID
         echo "freqtrade trade stopped."
-        break
-        return 1
+        exit 0
       fi
     done
 
