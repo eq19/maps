d941a21896c836a34cb536a806a1b4e11c8f3748
eQuantum <8466209+eq19@users.noreply.github.com>
2026-07-10 01:19:15 +0700

    Change epochs value from 640 to 3200

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -522,7 +522,7 @@ freqai() {
   jq -c --argjson ids "[$(echo "$*" | sed 's/ /,/g')]" '.pipelines[] | select(.id as $id | $ids | index($id))' $HYPERFILE | while read -r pipeline; do
 
     days=60
-    epochs=640
+    epochs=3200
 
     start_date=$EARLIEST_DATE
     end_date=$BACKTESTING_START
