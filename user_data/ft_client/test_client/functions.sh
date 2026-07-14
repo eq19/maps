bde6f4a60b2e12fb91437a631404dca62d051553
eQuantum <8466209+eq19@users.noreply.github.com>
2026-07-14 18:29:39 +0700

    Add freqtrade hyperopt-show command
    Add command to show hyperopt results after listing best options.

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -592,6 +592,7 @@ freqai() {
     echo $! > freqtrade_pid.txt
     monitor_freqtrade
     freqtrade hyperopt-list --best
+    freqtrade hyperopt-show
 
     echo -e "\n$hr\nRUN BACKTEST with $FREQAI_MODEL\n$hr"
     #Ref: https://www.freqtrade.io/en/stable/backtesting
