e1696d882ae6385668e32a2522562b657977555b
eQuantum <8466209+eq19@users.noreply.github.com>
2026-09-19 02:36:31 +0000

    Add retrieval and setting of PARAMS_JSON variable
    Retrieve and set PARAMS_JSON using PARAMS_LIVE from GitHub.

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -49,6 +49,8 @@ calculate_score() {
         HAS_FREQAI_TAGS="true"
       else
         echo "❌ No FreqAI tags detected in this backtest! Ignoring score.."
+        PARAMS_LIVE=$(gh variable get PARAMS_LIVE --repo "ZeoWeak/Spectra" --json value -q .value)
+        gh variable set PARAMS_JSON --repo "$TARGET_REPOSITORY" --body "$PARAMS_LIVE"
         HAS_FREQAI_TAGS="false"
         CALCULATION="true"
         return 0
