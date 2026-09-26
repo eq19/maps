ca791823698b55776e0669f66bf7a12e2b4817f5
eQuantum <8466209+eq19@users.noreply.github.com>
2026-09-26 03:47:55 +0000

    Change days to 7 and modify epochs in functions.sh
    Updated the 'days' variable to 7 and adjusted 'epochs' values in two instances.

--- user_data/ft_client/test_client/functions.sh
+++ user_data/ft_client/test_client/functions.sh
@@ -394,7 +394,7 @@ hyperopt() {
   # Load JSON and filter by given ID
   jq -c --argjson ids "[$(echo "$*" | sed 's/ /,/g')]" '.pipelines[] | select(.id as $id | $ids | index($id))' $HYPERFILE | while read -r pipeline; do
 
-    days=60
+    days=7
     epochs=1600
 
     start_date=$EARLIEST_DATE
@@ -520,8 +520,8 @@ freqai() {
   # Load JSON and filter by given ID
   jq -c --argjson ids "[$(echo "$*" | sed 's/ /,/g')]" '.pipelines[] | select(.id as $id | $ids | index($id))' $HYPERFILE | while read -r pipeline; do
 
-    days=60
-    epochs=3200
+    days=7
+    epochs=1600
 
     start_date=$EARLIEST_DATE
     end_date=$BACKTESTING_START
